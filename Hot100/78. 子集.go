8b1961576c4a1d1546c033131a3bca13dd03aa50
v_yuanwwu <nettywu@21kunpeng.com>
2021-06-08 07:47:24 +0800

    子集

--- Hot100/78. 子集.go
+++ Hot100/78. 子集.go
@@ -11,7 +11,7 @@ func main() {
 	fmt.Println(subsets([]int{1, 2, 3}))
 }
 
-// 类似offer 38
+// 排列组合，类似offer 38
 // 回溯法
 func subsets(nums []int) [][]int {
 	if nums == nil || len(nums) == 0 {
@@ -20,11 +20,45 @@ func subsets(nums []int) [][]int {
 
 	result := make([][]int, 0)
 	res := make([]int, 0)
-	dfs5(nums, 0, &res, &result)
+
+	for i := 0; i <= len(nums); i++ {
+		dfs5(nums, 0, i, &res, &result)
+	}
 	return result
 }
 
-func dfs5(nums []int, index int, res *[]int, result *[][]int) {
+// index: 从哪个数字开始
+// num: 这次要组合几个数字
+func dfs5(nums []int, index int, num int, res *[]int, result *[][]int) {
+	if len(*res) == num {
+		tmp := make([]int, num)
+		copy(tmp, *res)
+		*result = append(*result, tmp)
+
+		return
+	}
+
+	for i := index; i < len(nums); i++ {
+		*res = append(*res, nums[i])
+		dfs5(nums, i+1, num, res, result)
+		*res = (*res)[:len(*res)-1]
+	}
+}
+
+// 类似offer 38
+// 回溯法
+func subsets1(nums []int) [][]int {
+	if nums == nil || len(nums) == 0 {
+		return nil
+	}
+
+	result := make([][]int, 0)
+	res := make([]int, 0)
+	dfs6(nums, 0, &res, &result)
+	return result
+}
+
+func dfs6(nums []int, index int, res *[]int, result *[][]int) {
 
 	// 写入最终结果
 	tmp := make([]int, len(*res))
@@ -34,7 +68,7 @@ func dfs5(nums []int, index int, res *[]int, result *[][]int) {
 	// 遍历下一层
 	for i := index; i < len(nums); i++ {
 		*res = append(*res, nums[i])
-		dfs5(nums, i+1, res, result)
+		dfs6(nums, i+1, res, result)
 		*res = (*res)[:len(*res)-1]
 	}
 }
