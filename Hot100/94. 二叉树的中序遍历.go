3aa98b681281db81397d92b42da2351fb173e774
v_yuanwwu <nettywu@21kunpeng.com>
2021-06-09 08:05:02 +0800

    二叉树的中序遍历

---
+++ Hot100/94. 二叉树的中序遍历.go
@@ -0,0 +1,29 @@
+package main
+
+// 给定一个二叉树的根节点 root ，返回它的 中序 遍历。
+
+func main() {
+
+}
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func inorderTraversal(root *TreeNode) []int {
+	result := make([]int, 0)
+	inorder(root, &result)
+	return result
+}
+
+func inorder(node *TreeNode, result *[]int) {
+	if node == nil {
+		return
+	}
+
+	inorder(node.Left, result)
+	*result = append(*result, node.Val)
+	inorder(node.Right, result)
+}
