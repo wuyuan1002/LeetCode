a7348003eb04cc2dc6124dc5872e9832acf1857c
v_yuanwwu <nettywu@21kunpeng.com>
2021-06-14 09:46:25 +0800

    二叉树的最近公共祖先

---
+++ Hot100/236. 二叉树的最近公共祖先.go
@@ -0,0 +1,32 @@
+package main
+
+// 236. 二叉树的最近公共祖先
+
+// 给定一个二叉树, 找到该树中两个指定节点的最近公共祖先。
+//
+// 百度百科中最近公共祖先的定义为：“对于有根树 T 的两个节点 p、q，
+// 最近公共祖先表示为一个节点 x，满足 x 是 p、q 的祖先且 x 的深度尽可能大（一个节点也可以是它自己的祖先）。”
+
+func main() {
+
+}
+
+// 同offer 68
+func lowestCommonAncestor(root, p, q *TreeNode) *TreeNode {
+	if root == nil || root == p || root == q {
+		return root
+	}
+
+	// 后序遍历二叉树，从底向上寻找第一个p和q在它两侧的root节点
+	left := lowestCommonAncestor(root.Left, p, q)
+	right := lowestCommonAncestor(root.Right, p, q)
+
+	// 若p和q分别在root的两侧，则root为最近公共祖先
+	if left == nil {
+		return right
+	} else if right == nil {
+		return left
+	} else {
+		return root
+	}
+}
