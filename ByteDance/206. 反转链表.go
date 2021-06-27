1c4e426d8da766c989599b6e9daa0762f7c271c2
v_yuanwwu <nettywu@21kunpeng.com>
2021-06-27 08:52:02 +0800

    反转链表

---
+++ ByteDance/206. 反转链表.go
@@ -0,0 +1,32 @@
+package main
+
+// 206. 反转链表
+
+// 给你单链表的头节点 head ，请你反转链表，并返回反转后的链表。
+
+func main() {
+
+}
+
+func reverseList(head *ListNode) *ListNode {
+	if head == nil {
+		return nil
+	}
+
+	var pre, node, next *ListNode = nil, head, head
+	for node != nil {
+		next = node.Next
+
+		node.Next = pre
+
+		pre = node
+		node = next
+	}
+
+	return pre
+}
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
