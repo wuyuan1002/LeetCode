7cef2448cf726af9e462ccb663b49093f332532e
v_yuanwwu <nettywu@21kunpeng.com>
2021-07-19 09:00:56 +0800

    将有序数组转换为二叉搜索树

---
+++ top_interview/108. 将有序数组转换为二叉搜索树.go
@@ -0,0 +1,34 @@
+package main
+
+// 108. 将有序数组转换为二叉搜索树
+
+// 给你一个整数数组 nums ，其中元素已经按 升序 排列，请你将其转换为一棵 高度平衡 二叉搜索树。
+//
+// 高度平衡 二叉树是一棵满足「每个节点的左右两个子树的高度差的绝对值不超过 1 」的二叉树。
+
+func main() {
+
+}
+
+// 给了二叉搜索树的前序遍历序列，构建平衡的二叉搜索树
+// 因此，先找到中点作为根节点, 前半部分为左子树，后半部分为右子树
+func sortedArrayToBST(nums []int) *TreeNode {
+	if nums == nil || len(nums) == 0 {
+		return nil
+	}
+
+	mid := len(nums) / 2
+	node := &TreeNode{
+		Val:   nums[mid],
+		Left:  sortedArrayToBST(nums[:mid]),
+		Right: sortedArrayToBST(nums[mid+1:]),
+	}
+
+	return node
+}
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
