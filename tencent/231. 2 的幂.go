bc3f9d83aff88c62f375fd2c2a50e1c71e8d2ee3
v_yuanwwu <nettywu@21kunpeng.com>
2021-07-22 08:18:29 +0800

    2的幂

--- tencent/231. 2 的幂.go
+++ tencent/231. 2 的幂.go
@@ -11,22 +11,7 @@ func main() {
 }
 
 // 类似 offer15 二进制中1的个数
-// 若n的二进制位中初最低位外只有一个1就说明n是2的幂次方
+// 若n为正整数且二进制位中只有一个1就说明n是2的幂次方
 func isPowerOfTwo(n int) bool {
-	if n == 1 {
-		return true
-	}
-	if n == 0 || n&0x1 == 1 {
-		// 若n的第一位为1，说明n为奇数，肯定不是2的幂次方
-		return false
-	}
-
-	// 判断n的二进制中1的个数是否只有一个
-	count := 0
-	for n != 0 {
-		count++
-		n &= n - 1
-	}
-
-	return count == 1
+	return n > 0 && n&(n-1) == 0
 }
