a9d816fc6fa23121b335796a3e4ffbf2c7514fca
v_yuanwwu <2461190767@qq.com>
2021-07-26 09:24:12 +0800

    螺旋矩阵

--- byte_dance/54. 螺旋矩阵.go
+++ byte_dance/54. 螺旋矩阵.go
@@ -54,3 +54,54 @@ func printCircle(matrix [][]int, start int, result *[]int) {
 		}
 	}
 }
+
+// 第二次做
+func spiralOrder1(matrix [][]int) []int {
+	if matrix == nil || len(matrix) == 0 {
+		return nil
+	}
+
+	min := func(a, b int) int {
+		if a <= b {
+			return a
+		}
+		return b
+	}
+
+	result := make([]int, 0, len(matrix)*len(matrix[0]))   // 总结果
+	orderCount := min(len(matrix)+1, len(matrix[0])+1) / 2 // 要打印的圈数
+	for i := 0; i < orderCount; i++ {
+		printOrder(matrix, i, &result)
+	}
+
+	return result
+}
+
+func printOrder(matrix [][]int, start int, result *[]int) {
+	endX := len(matrix[0]) - start - 1 // 该圈最大横坐标
+	endY := len(matrix) - start - 1    // 该圈最大纵坐标
+
+	// 打印上面一行
+	for i := start; i <= endX; i++ {
+		*result = append(*result, matrix[start][i])
+	}
+
+	// 打印右面一列
+	for j := start + 1; j <= endY; j++ {
+		*result = append(*result, matrix[j][endX])
+	}
+
+	// 打印下面一行 -- 如果有的话
+	if endY > start {
+		for i := endX - 1; i >= start; i-- {
+			*result = append(*result, matrix[endY][i])
+		}
+	}
+
+	// 打印左面一列 -- 如果有的话
+	if endX > start {
+		for j := endY - 1; j >= start+1; j-- {
+			*result = append(*result, matrix[j][start])
+		}
+	}
+}
