43f62cf4337216dd3a1213402062458cdf0432c4
v_yuanwwu <2461190767@qq.com>
2021-08-01 11:19:55 +0800

    搜索旋转排序数组

---
+++ leetcode/33. 搜索旋转排序数组.go
@@ -0,0 +1,39 @@
+package main
+
+// 33. 搜索旋转排序数组
+
+func main() {
+
+}
+
+// offer 11
+// 二分法
+func search(nums []int, target int) int {
+	if nums == nil || len(nums) == 0 {
+		return -1
+	}
+
+	l, r := 0, len(nums)-1
+	for l <= r {
+		mid := (l + r) / 2
+		if nums[mid] == target {
+			return mid
+		}
+
+		if nums[0] <= nums[mid] { // 若左面是排好序的
+			if nums[mid] > target && nums[0] <= target {
+				r = mid - 1
+			} else {
+				l = mid + 1
+			}
+		} else { // 若右面是排好序的
+			if nums[mid] < target && nums[len(nums)-1] >= target {
+				l = mid + 1
+			} else {
+				r = mid - 1
+			}
+		}
+	}
+
+	return -1
+}
