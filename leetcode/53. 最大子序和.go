9bd7620cf1398dc722b811e74b8d4c72d4008356
v_yuanwwu <2461190767@qq.com>
2021-08-03 08:55:12 +0800

    最大子序和

---
+++ leetcode/53. 最大子序和.go
@@ -0,0 +1,28 @@
+package main
+
+// 53. 最大子序和
+
+func main() {
+
+}
+
+func maxSubArray(nums []int) int {
+	if nums == nil || len(nums) == 0 {
+		return 0
+	}
+	max := func(a, b int) int {
+		if a >= b {
+			return a
+		}
+		return b
+	}
+
+	sum := nums[0]
+	maxSum := sum
+	for i := 1; i < len(nums); i++ {
+		sum = max(sum+nums[i], nums[i])
+		maxSum = max(maxSum, sum)
+	}
+
+	return maxSum
+}
