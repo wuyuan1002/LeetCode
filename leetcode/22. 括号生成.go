520c5d4a9d5326fd8d73660e7538e7ec7c4d56d4
v_yuanwwu <2461190767@qq.com>
2021-08-09 08:50:32 +0800

    括号生成

---
+++ leetcode/22. 括号生成.go
@@ -0,0 +1,36 @@
+package main
+
+// 22. 括号生成
+
+func main() {
+
+}
+
+func generateParenthesis(n int) []string {
+	if n <= 0 {
+		return nil
+	}
+
+	res := make([]byte, 0, 2*n)
+	result := make([]string, 0, n)
+	dfs10(0, 0, n, &res, &result)
+	return result
+}
+
+func dfs10(left, right, n int, res *[]byte, result *[]string) {
+	if left == n && right == n {
+		*result = append(*result, string(*res))
+		return
+	}
+
+	if left < n {
+		*res = append(*res, '(')
+		dfs10(left+1, right, n, res, result)
+		*res = (*res)[:len(*res)-1]
+	}
+	if right < left {
+		*res = append(*res, ')')
+		dfs10(left, right+1, n, res, result)
+		*res = (*res)[:len(*res)-1]
+	}
+}
