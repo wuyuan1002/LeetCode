40a624e6d36fb9664dd8cb3f883c2788523c79e3
v_yuanwwu <2461190767@qq.com>
2021-08-11 07:57:59 +0800

    最长回文子串

---
+++ leetcode/5. 最长回文子串.go
@@ -0,0 +1,35 @@
+package main
+
+import "fmt"
+
+// 5. 最长回文子串
+
+func main() {
+	fmt.Println(longestPalindrome("babad"))
+}
+
+// 中心扩散法
+func longestPalindrome(s string) string {
+	if len(s) < 2 {
+		return s
+	}
+
+	longest := "" // 最长回文子串
+	for i := 0; i < len(s); i++ {
+		l, r := i-1, i+1
+		for l >= 0 && s[l] == s[i] {
+			l--
+		}
+		for r < len(s) && s[r] == s[i] {
+			r++
+		}
+		for l >= 0 && r < len(s) && s[l] == s[r] {
+			l--
+			r++
+		}
+		if r-l-1 > len(longest) {
+			longest = s[l+1 : r]
+		}
+	}
+	return longest
+}
