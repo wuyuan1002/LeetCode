392846aea8ee5c694c68c8fabb906a26e8f615a7
v_yuanwwu <2461190767@qq.com>
2021-08-12 09:02:17 +0800

    无重复字符的最长子串

---
+++ leetcode/3. 无重复字符的最长子串.go
@@ -0,0 +1,26 @@
+package main
+
+// 3. 无重复字符的最长子串
+
+func main() {
+
+}
+
+// 滑动窗口，同时使用map存储字符出现过的下标
+func lengthOfLongestSubstring(s string) int {
+	if s == "" {
+		return 0
+	}
+
+	hash := make(map[byte]int, 0)
+	maxLen := 0
+	l := 0
+	for i, c := range s {
+		if index, ok := hash[byte(c)]; ok {
+			l = max(l, index+1)
+		}
+		hash[byte(c)] = i
+		maxLen = max(maxLen, i-l+1)
+	}
+	return maxLen
+}
