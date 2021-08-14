76acc6ed3a7f3e063e8f97bd609aeaf0d4299cbe
v_yuanwwu <2461190767@qq.com>
2021-08-14 09:40:43 +0800

    盛最多水的容器

--- leetcode/11. 盛最多水的容器.go
+++ leetcode/11. 盛最多水的容器.go
@@ -33,3 +33,30 @@ func maxArea(height []int) int {
 
 	return res
 }
+
+// 双指针法，两个指针向中间移动，每次都是移动数字小的那个
+func maxArea1(height []int) int {
+	if height == nil || len(height) == 0 {
+		return 0
+	}
+	max := func(a, b int) int {
+		if a >= b {
+			return a
+		}
+		return b
+	}
+
+	res := 0
+	l, r := 0, len(height)-1
+	for l < r {
+		if height[l] < height[r] {
+			res = max(res, height[l]*(r-l))
+			l++
+		} else {
+			res = max(res, height[r]*(r-l))
+			r--
+		}
+	}
+
+	return res
+}
