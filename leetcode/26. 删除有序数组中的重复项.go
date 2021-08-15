be732494b72915df1959c97aab193e9b200e8a7e
v_yuanwwu <2461190767@qq.com>
2021-08-15 09:39:55 +0800

    删除有序数组中的重复项

---
+++ leetcode/26. 删除有序数组中的重复项.go
@@ -0,0 +1,24 @@
+package main
+
+// 26. 删除有序数组中的重复项
+
+func main() {
+
+}
+
+// 双指针
+func removeDuplicates1(nums []int) int {
+	if nums == nil || len(nums) == 0 {
+		return 0
+	}
+
+	l, r := 0, 0
+	for r < len(nums) {
+		if nums[r] != nums[l] {
+			l++
+			nums[l] = nums[r]
+		}
+		r++
+	}
+	return l + 1
+}
