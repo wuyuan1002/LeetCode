8205d92ca1a80afa2ae85789865c74114024992b
v_yuanwwu <2461190767@qq.com>
2021-08-15 09:31:29 +0800

    二分查找

---
+++ leetcode/704. 二分查找.go
@@ -0,0 +1,29 @@
+package main
+
+// 704. 二分查找
+
+// 给定一个n个元素有序的（升序）整型数组nums 和一个目标值target ，
+// 写一个函数搜索nums中的 target，如果目标值存在返回下标，否则返回 -1。
+
+func main() {
+
+}
+
+func search2(nums []int, target int) int {
+	if nums == nil || len(nums) == 0 {
+		return -1
+	}
+
+	l, r := 0, len(nums)-1
+	for l <= r {
+		mid := (l + r) / 2
+		if nums[mid] > target {
+			r = mid - 1
+		} else if nums[mid] < target {
+			l = mid + 1
+		} else {
+			return mid
+		}
+	}
+	return -1
+}
