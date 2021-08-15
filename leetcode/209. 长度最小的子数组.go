e4bc3eed9a45516ea5ea02c35ee18a7db23bb289
v_yuanwwu <2461190767@qq.com>
2021-08-15 10:07:41 +0800

    长度最小的子数组

--- leetcode/209. 长度最小的子数组.go
+++ leetcode/209. 长度最小的子数组.go
@@ -38,3 +38,33 @@ func minSubArrayLen(target int, nums []int) int {
 	}
 	return minLen
 }
+
+// 第二次做
+func minSubArrayLen1(target int, nums []int) int {
+	if nums == nil || len(nums) == 0 {
+		return 0
+	}
+
+	minLen := math.MaxInt32
+	sum := 0
+	l, r := 0, 0
+	for ; r < len(nums); r++ {
+		sum += nums[r]
+		for ; sum >= target && l <= r; l++ {
+			minLen = min(minLen, r-l+1)
+			sum -= nums[l]
+		}
+	}
+
+	if minLen == math.MaxInt32 {
+		return 0
+	}
+	return minLen
+}
+
+func min(a, b int) int {
+	if a <= b {
+		return a
+	}
+	return b
+}
