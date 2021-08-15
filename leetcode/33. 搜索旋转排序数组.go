9d2c87781bbd56a350178e44fa651e1fb042cd9e
v_yuanwwu <2461190767@qq.com>
2021-08-15 09:08:43 +0800

    搜索旋转排序数组

--- leetcode/33. 搜索旋转排序数组.go
+++ leetcode/33. 搜索旋转排序数组.go
@@ -37,3 +37,35 @@ func search(nums []int, target int) int {
 
 	return -1
 }
+
+// offer 11
+// 二分法
+func search1(nums []int, target int) int {
+	if nums == nil || len(nums) == 0 {
+		return -1
+	}
+
+	l, r := 0, len(nums)-1
+	for l <= r {
+		mid := (l + r) / 2
+		if nums[mid] == target {
+			return mid
+		}
+
+		if nums[0] <= nums[mid] {
+			if nums[0] <= target && nums[mid] > target {
+				r = mid - 1
+			} else {
+				l = mid + 1
+			}
+		} else {
+			if nums[len(nums)-1] >= target && nums[mid] < target {
+				l = mid + 1
+			} else {
+				r = mid - 1
+			}
+		}
+	}
+
+	return -1
+}
