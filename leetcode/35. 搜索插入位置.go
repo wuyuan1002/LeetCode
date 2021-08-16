92cb82cab6e156ad2eb33162ae8f14d9cf4e5635
v_yuanwwu <2461190767@qq.com>
2021-08-16 08:36:41 +0800

    搜索插入位置

---
+++ leetcode/35. 搜索插入位置.go
@@ -0,0 +1,32 @@
+package main
+
+// 35. 搜索插入位置
+
+// 给定一个排序数组和一个目标值，在数组中找到目标值，并返回其索引。
+// 如果目标值不存在于数组中，返回它将会被按顺序插入的位置。
+//
+// 请必须使用时间复杂度为 O(log n) 的算法。
+
+func main() {
+
+}
+
+// 二分法
+func searchInsert(nums []int, target int) int {
+	if nums == nil || len(nums) == 0 {
+		return -1
+	}
+
+	l, r := 0, len(nums)-1
+	for l <= r {
+		mid := (l + r) / 2
+		if nums[mid] > target {
+			r = mid - 1
+		} else if nums[mid] < target {
+			l = mid + 1
+		} else {
+			return mid
+		}
+	}
+	return l
+}
