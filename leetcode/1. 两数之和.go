3608ddeadd31e2ab0db7ddff1439818f1458c09e
v_yuanwwu <2461190767@qq.com>
2021-08-18 07:55:44 +0800

    快乐数、两数之和

--- leetcode/1. 两数之和.go
+++ leetcode/1. 两数之和.go
@@ -21,3 +21,14 @@ func twoSum(nums []int, target int) []int {
 	}
 	return nil
 }
+
+func twoSum2(nums []int, target int) []int {
+	hash := make(map[int]int)
+	for i, num := range nums {
+		if index, ok := hash[target-num]; ok {
+			return []int{index, i}
+		}
+		hash[num] = i
+	}
+	return nil
+}
---
+++ leetcode/202. 快乐数.go
@@ -0,0 +1,40 @@
+package main
+
+// 202. 快乐数
+
+// 编写一个算法来判断一个数 n 是不是快乐数。
+//
+// 「快乐数」定义为：
+// 对于一个正整数，每一次将该数替换为它每个位置上的数字的平方和。
+// 然后重复这个过程直到这个数变为 1，也可能是 无限循环 但始终变不到 1。
+// 如果 可以变为 1，那么这个数就是快乐数。
+// 如果 n 是快乐数就返回 true ；不是，则返回 false 。
+
+func main() {
+
+}
+
+// 使用map存某依次的计算结果是否出现过，若出现过，则说明将会无限循环永远变不成1
+func isHappy(n int) bool {
+	mapp := make(map[int]struct{})
+	getSum := func(num int) int { // 获取num每一位的平方和
+		sum := 0
+		for num != 0 {
+			n := num % 10
+			num /= 10
+			sum += n * n
+		}
+		return sum
+	}
+
+	num := n
+	for {
+		num = getSum(num)
+		if num == 1 {
+			return true
+		} else if _, ok := mapp[num]; ok {
+			return false
+		}
+		mapp[num] = struct{}{}
+	}
+}
