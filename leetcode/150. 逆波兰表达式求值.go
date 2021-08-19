bed567f1355610710b5b7a27c87aaa6bca3b2b32
v_yuanwwu <2461190767@qq.com>
2021-08-19 09:06:45 +0800

    逆波兰表达式求值

---
+++ leetcode/150. 逆波兰表达式求值.go
@@ -0,0 +1,38 @@
+package main
+
+import "strconv"
+
+// 150. 逆波兰表达式求值
+
+// 根据 逆波兰表示法，求表达式的值。
+//
+// 有效的算符包括 +、-、*、/ 。每个运算对象可以是整数，也可以是另一个逆波兰表达式。
+
+func main() {
+
+}
+
+// 遇到数字则入栈；遇到算符则取出栈顶两个数字进行计算，并将结果压入栈中
+func evalRPN(tokens []string) int {
+	stack := make([]int, 0)
+	for _, token := range tokens {
+		val, err := strconv.Atoi(token)
+		if err == nil {
+			stack = append(stack, val)
+		} else {
+			num1, num2 := stack[len(stack)-2], stack[(len(stack))-1]
+			stack = stack[:len(stack)-2]
+			switch token {
+			case "+":
+				stack = append(stack, num1+num2)
+			case "-":
+				stack = append(stack, num1-num2)
+			case "*":
+				stack = append(stack, num1*num2)
+			case "/":
+				stack = append(stack, num1/num2)
+			}
+		}
+	}
+	return stack[0]
+}
