9637186c36cba505d166de9d7a8cedba529b1fd5
v_yuanwwu <2461190767@qq.com>
2021-08-28 10:10:52 +0800

    判断子序列

---
+++ leetcode/392. 判断子序列.go
@@ -0,0 +1,51 @@
+package main
+
+import "fmt"
+
+// 392. 判断子序列
+
+// 给定字符串 s 和 t ，判断 s 是否为 t 的子序列。
+//
+// 字符串的一个子序列是原始字符串删除一些（也可以不删除）字符而不改变剩余字符相对位置形成的新字符串。
+// （例如，"ace"是"abcde"的一个子序列，而"aec"不是）。
+//
+// 进阶：
+//
+// 如果有大量输入的 S，称作 S1, S2, ... , Sk 其中 k >= 10亿，
+// 你需要依次检查它们是否为 T 的子序列。在这种情况下，你会怎样改变代码？
+
+func main() {
+	fmt.Println(isSubsequence("abc", "ahbgdc"))
+}
+
+// 1. 双指针
+// 2. 动态规划
+func isSubsequence(s string, t string) bool {
+	i, j := 0, 0
+	for i < len(s) && j < len(t) {
+		if s[i] == t[j] {
+			i++
+		}
+		j++
+	}
+	return i == len(s)
+}
+
+func isSubsequence1(s string, t string) bool {
+	dp := make([][]int, len(s)+1)
+	for i := range dp {
+		dp[i] = make([]int, len(t)+1)
+	}
+
+	for i := 1; i <= len(s); i++ {
+		for j := 1; j <= len(t); j++ {
+			if s[i-1] == t[j-1] {
+				dp[i][j] = dp[i-1][j-1] + 1
+			} else {
+				dp[i][j] = max(dp[i-1][j], dp[i][j-1])
+			}
+		}
+	}
+
+	return dp[len(s)][len(t)] == len(s)
+}
