ea06ea6efd8ff0439b641da1168290f7641785f3
v_yuanwwu <2461190767@qq.com>
2021-09-04 11:05:08 +0800

    不同的二叉搜索树

--- leetcode/96. 不同的二叉搜索树.go
+++ leetcode/96. 不同的二叉搜索树.go
@@ -20,3 +20,15 @@ func numTrees(n int) int {
 	}
 	return dp[n]
 }
+
+// 第二次做
+func numTrees1(n int) int {
+	dp := make([]int, n+1)
+	dp[0] = 1
+	for i := 1; i <= n; i++ {
+		for j := 1; j <= i; j++ {
+			dp[i] += dp[j-1] * dp[i-j]
+		}
+	}
+	return dp[n]
+}
