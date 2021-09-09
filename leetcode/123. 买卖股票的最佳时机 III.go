d53eaa5f7c56fb865846c65bfa1ace2489464ec4
v_yuanwwu <2461190767@qq.com>
2021-09-09 08:43:45 +0800

    log

--- leetcode/123. 买卖股票的最佳时机 III.go
+++ leetcode/123. 买卖股票的最佳时机 III.go
@@ -20,7 +20,7 @@ func main() {
 // 2. 第一次卖出
 // 3. 第二次买入
 // 4. 第二次卖出
-// dp[i][j]中 i表示第i天，j为 [0 - 4] 五个状态，dp[i][j]表示第i天状态j所剩最大现金。
+// dp[i][j]中 i表示第i天，j为 [0 - 4] 五个状态，dp[i][j]表示第i天状态j时所持有的现金数量(越大说明越有钱)
 func maxProfit2(prices []int) int {
 	if prices == nil || len(prices) == 0 {
 		return 0
@@ -40,12 +40,13 @@ func maxProfit2(prices []int) int {
 
 	// 遍历数组
 	for i := 1; i < len(prices); i++ {
-		dp[i][0] = dp[i-1][0]
-		dp[i][1] = max(dp[i-1][0]-prices[i], dp[i-1][1])
-		dp[i][2] = max(dp[i-1][1]+prices[i], dp[i-1][2])
-		dp[i][3] = max(dp[i-1][2]-prices[i], dp[i-1][3])
-		dp[i][4] = max(dp[i-1][3]+prices[i], dp[i-1][4])
+		dp[i][0] = dp[i-1][0]                            // 保持前一天的未操作状态
+		dp[i][1] = max(dp[i-1][0]-prices[i], dp[i-1][1]) // 保持前一天的买入状态或在前一天没有买入时进行买入
+		dp[i][2] = max(dp[i-1][1]+prices[i], dp[i-1][2]) // 保持前一天的卖出状态或在前一天没有卖出时卖出
+		dp[i][3] = max(dp[i-1][2]-prices[i], dp[i-1][3]) // 保持前一天的买入状态或在前一天没有买入时进行买入
+		dp[i][4] = max(dp[i-1][3]+prices[i], dp[i-1][4]) // 保持前一天的卖出状态或在前一天没有卖出时卖出
 	}
 
+	// 返回最后一天第二次卖出后所持有的现金数量
 	return dp[len(prices)-1][4]
 }
