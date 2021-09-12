e460bb9c00081ad766bdbf2f85080a8592a0c739
v_yuanwwu <2461190767@qq.com>
2021-09-12 17:45:03 +0800

    最长递增子序列

--- leetcode/300. 最长递增子序列.go
+++ leetcode/300. 最长递增子序列.go
@@ -59,3 +59,26 @@ func lengthOfLIS1(nums []int) int {
 	}
 	return maxLen
 }
+
+func lengthOfLIS2(nums []int) int {
+	if nums == nil || len(nums) == 0 {
+		return 0
+	}
+
+	dp := make([]int, len(nums))
+	for i := range nums {
+		dp[i] = 1
+	}
+
+	maxLen := 0
+	for i := 0; i < len(nums); i++ {
+		for j := 0; j < i; j++ {
+			if nums[i] > nums[j] {
+				dp[i] = max(dp[i], dp[j]+1)
+			}
+		}
+		maxLen = max(maxLen, dp[i])
+	}
+
+	return maxLen
+}
