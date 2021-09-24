5c36c7cc4cbc10578298cc59acd1dfa58405e413
v_yuanwwu <2461190767@qq.com>
2021-09-24 09:23:11 +0800

    最后一个单词的长度

---
+++ leetcode/58. 最后一个单词的长度.go
@@ -0,0 +1,27 @@
+package main
+
+// 58. 最后一个单词的长度
+
+// 给你一个字符串 s，由若干单词组成，单词前后用一些空格字符隔开。返回字符串中最后一个单词的长度。
+//
+// 单词 是指仅由字母组成、不包含任何空格字符的最大子字符串。
+
+func main() {
+
+}
+
+func lengthOfLastWord(s string) int {
+	if s == "" {
+		return 0
+	}
+
+	length := 0
+	for i := len(s) - 1; i >= 0; i-- {
+		if s[i] == ' ' && length > 0 {
+			break
+		} else if s[i] != ' ' {
+			length++
+		}
+	}
+	return length
+}
