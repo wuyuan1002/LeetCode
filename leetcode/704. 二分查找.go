6c8596ecb9f01c17b68369cf4abb8e2ebd2978ab
v_yuanwwu <2461190767@qq.com>
2021-09-26 11:05:10 +0800

    log

--- leetcode/704. 二分查找.go
+++ leetcode/704. 二分查找.go
@@ -16,7 +16,8 @@ func search2(nums []int, target int) int {
 
 	l, r := 0, len(nums)-1
 	for l <= r {
-		mid := (l + r) / 2
+		// mid := (l + r) / 2
+		mid := l + (r-l)/2 // 防止l+r时超出int的范围
 		if nums[mid] > target {
 			r = mid - 1
 		} else if nums[mid] < target {
