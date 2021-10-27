8a714790fe3b7ebaeb3370ac236d1b21cd3d9537
wuyuan <2461190767@qq.com>
2021-10-27 08:43:58 +0800

    log

--- leetcode/496. 下一个更大元素 I.go
+++ leetcode/496. 下一个更大元素 I.go
@@ -13,12 +13,13 @@ func main() {
 
 // 一但要求下一个更大的元素，就是用单调栈解，leetcode相似的题目都是这个解法
 
+// 同503，本题可以使用map来存数字的下一个更大元素，也可以直接再res中保存，如503那样
 // 单调栈 + map哈希表
 func nextGreaterElement(nums1 []int, nums2 []int) []int {
 	// 首先倒序遍历nums2，维护一个单调递减栈，同时使用map记录下nums2中每个数字的下一个更大元素
 	// 之后再遍历nums1，分别从map中获取对应数字的下一个更大元素
 	stack := make([]int, 0)
-	hash := make(map[int]int)
+	hash := make(map[int]int) // 保存元素与它的下一个更大元素
 	for i := len(nums2) - 1; i >= 0; i-- {
 		num := nums2[i]
 		// 若栈中元素有小于当前元素的，把他们出栈，此时，栈中下一个元素即为当前num的下一个更大元素
