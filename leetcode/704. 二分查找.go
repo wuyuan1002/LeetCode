ea6e56d44c2a8ed9af884476f8b2985bef45e07d
wuyuan <2461190767@qq.com>
2022-01-16 09:27:59 +0800

    二分查找

--- leetcode/704. 二分查找.go
+++ leetcode/704. 二分查找.go
@@ -28,3 +28,23 @@ func search2(nums []int, target int) int {
 	}
 	return -1
 }
+
+// 第二次做
+func search3(nums []int, target int) int {
+	if nums == nil || len(nums) == 0 {
+		return -1
+	}
+
+	l, r := 0, len(nums)-1
+	for l <= r {
+		mid := l + (r-l)/2
+		if nums[mid] < target {
+			l = mid + 1
+		} else if nums[mid] > target {
+			r = mid - 1
+		} else {
+			return mid
+		}
+	}
+	return -1
+}
