7107e7c03d3316230f1feff0aa2737525a45e08e
wuyuan <2461190767@qq.com>
2022-01-20 08:29:04 +0800

    在排序数组中查找元素的第一个和最后一个位置

--- leetcode/34. 在排序数组中查找元素的第一个和最后一个位置.go
+++ leetcode/34. 在排序数组中查找元素的第一个和最后一个位置.go
@@ -55,3 +55,46 @@ func getLast(nums []int, target int) int {
 	}
 	return -1
 }
+
+// 第二次做
+func searchRange1(nums []int, target int) []int {
+	return []int{getFirst1(nums, target), getLast1(nums, target)}
+}
+
+func getFirst1(nums []int, target int) int {
+	l, r := 0, len(nums)-1
+	for l <= r {
+		mid := l + (r-l)/2
+		if nums[mid] < target {
+			l = mid + 1
+		} else if nums[mid] > target {
+			r = mid - 1
+		} else {
+			if mid > 0 && nums[mid-1] == target {
+				r = mid - 1
+			} else {
+				return mid
+			}
+		}
+	}
+	return -1
+}
+
+func getLast1(nums []int, target int) int {
+	l, r := 0, len(nums)-1
+	for l <= r {
+		mid := l + (r-l)/2
+		if nums[mid] < target {
+			l = mid + 1
+		} else if nums[mid] > target {
+			r = mid - 1
+		} else {
+			if mid < len(nums)-1 && nums[mid+1] == target {
+				l = mid + 1
+			} else {
+				return mid
+			}
+		}
+	}
+	return -1
+}
