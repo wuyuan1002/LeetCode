3e283178a7a6920e208be984c4ab543dcfce0f05
wuyuan <2461190767@qq.com>
2022-01-25 08:22:54 +0800

    环形链表 II

--- leetcode/142. 环形链表 II.go
+++ leetcode/142. 环形链表 II.go
@@ -2,6 +2,14 @@ package main
 
 // 142. 环形链表 II
 
+// 给定一个链表，返回链表开始入环的第一个节点。 如果链表无环，则返回 null。
+//
+// 如果链表中有某个节点，可以通过连续跟踪 next 指针再次到达，则链表中存在环。
+// 为了表示给定链表中的环，评测系统内部使用整数 pos 来表示链表尾连接到链表中的位置（索引从 0 开始）。
+// 如果 pos 是 -1，则在该链表中没有环。注意: pos 不作为参数进行传递，仅仅是为了标识链表的实际情况。
+//
+// 不允许修改 链表。
+
 // func main() {
 
 // }
@@ -9,6 +17,25 @@ package main
 // 1. 遍历所有节点，使用map记录已经访问过的节点
 // 2. 快慢指针 -- 两指针相遇后，再定义一个慢指针从链表头出发，当快慢指针再次相遇时，它指向的就是环的入口
 func detectCycle(head *ListNode) *ListNode {
+	slow, fast := head, head
+	for fast != nil && fast.Next != nil {
+		slow = slow.Next
+		fast = fast.Next.Next
+
+		if slow == fast {
+			slow = head
+			for slow != fast {
+				slow = slow.Next
+				fast = fast.Next
+			}
+			return slow
+		}
+	}
+	return nil
+}
+
+// 第二次做
+func detectCycle1(head *ListNode) *ListNode {
 	slow, fast := head, head
 	for fast != nil && fast.Next != nil {
 		slow = slow.Next
