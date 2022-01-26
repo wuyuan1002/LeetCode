1ea8b6e1ae0ef822b1d60a451408c777c920c237
wuyuan <2461190767@qq.com>
2022-01-26 08:31:41 +0800

    字母异位词分组

--- leetcode/49. 字母异位词分组.go
+++ leetcode/49. 字母异位词分组.go
@@ -6,6 +6,9 @@ import (
 
 // 49. 字母异位词分组
 
+// 给你一个字符串数组，请你将 字母异位词 组合在一起。可以按任意顺序返回结果列表。
+// 字母异位词 是由重新排列源单词的字母得到的一个新单词，所有源单词中的字母通常恰好只用一次。
+
 // func main() {
 
 // }
@@ -27,5 +30,28 @@ func groupAnagrams(strs []string) [][]string {
 	for _, v := range mapp {
 		result = append(result, v)
 	}
+	return result
+}
+
+// 第二次做
+func groupAnagrams1(strs []string) [][]string {
+	if strs == nil || len(strs) == 0 {
+		return nil
+	}
+
+	result := make([][]string, 0)        // 总结果
+	mapp := make(map[string][]string, 0) // 存放字母异位词分组
+
+	for _, str := range strs {
+		s := []byte(str)
+		sort.Slice(s, func(a, b int) bool { return s[a] < s[b] })
+		bystr := string(s)
+		mapp[bystr] = append(mapp[bystr], str)
+	}
+
+	for _, v := range mapp {
+		result = append(result, v)
+	}
+
 	return result
 }
