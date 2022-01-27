cf43e6bb76ac21010d6d2e7409a2a518bd145be3
wuyuan <2461190767@qq.com>
2022-01-27 08:27:48 +0800

    两个数组的交集

--- leetcode/349. 两个数组的交集.go
+++ leetcode/349. 两个数组的交集.go
@@ -5,7 +5,7 @@ package main
 // 给定两个数组，编写一个函数来计算它们的交集。
 
 // func main() {
-
+// 	fmt.Println(intersection1([]int{1}, []int{1}))
 // }
 
 // 350
@@ -23,6 +23,32 @@ func intersection(nums1 []int, nums2 []int) []int {
 	hash2 := make(map[int]struct{}, 0)
 	for _, n := range nums2 {
 		hash2[n] = struct{}{}
+	}
+
+	result := make([]int, 0)
+	for n := range hash2 {
+		if _, ok := hash1[n]; ok {
+			result = append(result, n)
+		}
+	}
+
+	return result
+}
+
+// 第二次做
+func intersection1(nums1 []int, nums2 []int) []int {
+	if nums1 == nil || len(nums1) == 0 || nums2 == nil || len(nums2) == 0 {
+		return nil
+	}
+
+	hash1 := make(map[int]bool)
+	for _, n := range nums1 {
+		hash1[n] = true
+	}
+
+	hash2 := make(map[int]bool)
+	for _, n := range nums2 {
+		hash2[n] = true
 	}
 
 	result := make([]int, 0)
