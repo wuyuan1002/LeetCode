9bb8af3fd9dc33b8efd032bdf2928817d18a5786
wuyuan <2461190767@qq.com>
2022-03-19 11:27:06 +0800

    无重复字符的最长子串

--- leetcode/3. 无重复字符的最长子串.go
+++ leetcode/3. 无重复字符的最长子串.go
@@ -2,8 +2,12 @@ package main
 
 // 3. 无重复字符的最长子串
 
-// func main() {
+// 给定一个字符串，请你找出其中不含有重复字符的最长子串的长度。
 
+// func main() {
+// 	fmt.Println(lengthOfLongestSubstring("pwwkew"))
+// 	// fmt.Println(lengthOfLongestSubstring("abba"))
+// 	// fmt.Println(lengthOfLongestSubstring("abcabcbb"))
 // }
 
 // 滑动窗口，同时使用map存储字符出现过的下标
@@ -34,6 +38,23 @@ func lengthOfLongestSubstring1(s string) int {
 			l = max(l, index+1)
 		}
 		hash[byte(c)] = i
+		maxLen = max(maxLen, i-l+1)
+	}
+	return maxLen
+}
+
+func lengthOfLongestSubstring2(s string) int {
+	hash := make(map[byte]int)
+	maxLen := 0
+	l := 0
+	for i, c := range s {
+		if index, ok := hash[byte(c)]; ok {
+			// 若该字符已经存在，将左边界向前移动，不能直接移动到index+1，原因：abba
+			l = max(l, index+1)
+		}
+		// 将最新位置存入map
+		hash[byte(c)] = i
+		// 计算最大长度
 		maxLen = max(maxLen, i-l+1)
 	}
 	return maxLen
