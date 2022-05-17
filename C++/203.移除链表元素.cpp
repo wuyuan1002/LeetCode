b2f9124858c120d1dfc014b71c44633fd663f4d7
wuyuan <2461190767@qq.com>
2022-05-17 09:52:44 +0800

    反转链表

--- C++/203.移除链表元素.cpp
+++ C++/203.移除链表元素.cpp
@@ -3,21 +3,7 @@
 // 给你一个链表的头节点 head 和一个整数 val ，
 // 请你删除链表中所有满足 Node.val == val 的节点，并返回 新的头节点 。
 
-struct ListNode {
-    int val;
-    ListNode* next;
-
-    ListNode()
-        : val(0)
-        , next(nullptr) {};
-    ListNode(int x)
-        : val(x)
-        , next(nullptr) {};
-    ListNode(int x, ListNode* next)
-        : val(x)
-        , next(next) {};
-    ~ListNode() = default;
-};
+#include "header.h"
 
 class Solution {
 public:
