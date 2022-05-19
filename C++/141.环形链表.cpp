f38661bc264dbec618e861f0db2e337ef25e0617
wuyuan <2461190767@qq.com>
2022-05-19 09:06:10 +0800

    环形链表

---
+++ C++/141.环形链表.cpp
@@ -0,0 +1,23 @@
+
+// 141. 环形链表
+//
+// 给你一个链表的头节点 head ，判断链表中是否有环。
+
+#include <header.h>
+
+class Solution {
+public:
+    ListNode* hasCycle(ListNode* head)
+    {
+        ListNode *fast = head, *slow = head;
+        while (fast != nullptr && fast->next != nullptr) {
+            fast = fast->next->next;
+            slow = slow->next;
+
+            if (fast == slow) {
+                return fast;
+            }
+        }
+        return nullptr;
+    }
+};
