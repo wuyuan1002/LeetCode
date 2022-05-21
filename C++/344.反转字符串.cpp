7caea9065e8100050714b1c80c8dc1a82c875a7e
wuyuan <2461190767@qq.com>
2022-05-21 10:22:51 +0800

    反转字符串

---
+++ C++/344.反转字符串.cpp
@@ -0,0 +1,14 @@
+
+// 344. 反转字符串
+
+#include <vector>
+
+class Solution {
+public:
+    void reverseString(std::vector<char>& s)
+    {
+        for (int l = 0, r = s.size() - 1; l < r; ++l, --r) {
+            std::swap(s[l], s[r]);
+        }
+    }
+};
