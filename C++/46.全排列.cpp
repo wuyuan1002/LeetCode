2b800bc1792a928d73486b624d250f83b884bda3
wuyuan <2461190767@qq.com>
2022-05-22 20:45:18 +0800

    全排列

---
+++ C++/46.全排列.cpp
@@ -0,0 +1,40 @@
+
+// 46. 全排列
+//
+// 给定一个不含重复数字的数组 nums ，返回其 所有可能的全排列 。你可以 按任意顺序 返回答案。
+
+#include <algorithm>
+#include <unordered_set>
+#include <vector>
+
+class Solution {
+public:
+    // 回溯法
+    std::vector<std::vector<int>> permute(std::vector<int>& nums)
+    {
+        std::vector<std::vector<int>> result;
+        dfs(nums, 0, result);
+        return result;
+    }
+
+    void dfs(std::vector<int>& nums, int index, std::vector<std::vector<int>>& result)
+    {
+        if (index == nums.size()) {
+            result.push_back(std::vector<int>(nums));
+            return;
+        }
+
+        std::unordered_set<int> visited;
+        for (int i = index; i < nums.size(); ++i) {
+            if (visited.find(nums[i]) != visited.end()) {
+                continue;
+            }
+
+            visited.insert(nums[i]);
+
+            std::swap(nums[index], nums[i]);
+            dfs(nums, index + 1, result);
+            std::swap(nums[index], nums[i]);
+        }
+    }
+};
