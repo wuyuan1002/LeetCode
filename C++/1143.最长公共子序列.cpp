7f302ede78098a9c1eb48db92992e6db11e4fddc
wuyuan <2461190767@qq.com>
2022-07-10 10:06:29 +0800

    最长重复子数组

--- C++/1143.最长公共子序列.cpp
+++ C++/1143.最长公共子序列.cpp
@@ -25,7 +25,7 @@ public:
         // dp[i][j]表示以下标i-1结尾的text1和下标j-1结尾的text2的数组的字符串的公共子序列长度
         // dp[i][j] =
         // 若 A[i-1] == B[j-1], 则 dp[i][j] = dp[i-1][j-1] + 1
-        // 若 A[i-1] != B[j-1], 则 dp[i][j] = max(dp[i-1][j], dp[i][j-1])
+        // 若 A[i-1] != B[j-1], 则 dp[i][j] = max(dp[i-1][j], dp[i][j-1]) --> 公共子序列可以不连续, 因此不用像718那样使用0
         std::vector<std::vector<int>> dp(text1.size() + 1, std::vector<int>(text2.size() + 1, 0));
 
         for (int i = 1; i <= text1.size(); i++) {
--- byte_dance/718. 最长重复子数组.go
+++ byte_dance/718. 最长重复子数组.go
@@ -12,7 +12,7 @@ package main
 // 2. 滑动窗口
 
 // 类似于 1143. 最长公共子序列
-// 2. 二维dp，空间占用较大 -- dp[i][j]表示以下标i-1结尾的nums1和下标j-1结尾的nums2的数组的重复子数组长度
+// 1. 二维dp，空间占用较大 -- dp[i][j]表示以下标i-1结尾的nums1和下标j-1结尾的nums2的数组的重复子数组长度
 func findLength(nums1 []int, nums2 []int) int {
 	if nums1 == nil || len(nums1) == 0 || nums2 == nil || len(nums2) == 0 {
 		return 0
@@ -46,7 +46,7 @@ func findLength(nums1 []int, nums2 []int) int {
 	return maxLen
 }
 
-// 2. 滚动数组优化，一维dp，空间占用较小 -- 类似礼物的最大价值、零钱兑换、不同路径
+// 1. 滚动数组优化，一维dp，空间占用较小 -- 类似礼物的最大价值、零钱兑换、不同路径
 func findLength1(nums1 []int, nums2 []int) int {
 	if nums1 == nil || len(nums1) == 0 || nums2 == nil || len(nums2) == 0 {
 		return 0
