f48b43735f2e00af173a73f6723a66d18d7aeb1e
wuyuan <2461190767@qq.com>
2023-03-29 08:39:03 +0800

    19. 删除链表的倒数第 N 个结点

--- C++/19.删除链表的倒数第N个结点.cpp
+++ C++/19.删除链表的倒数第N个结点.cpp
@@ -9,21 +9,27 @@
 class Solution {
 public:
     // 双指针
+    // 先让右指针向后移动n, 之后左右指针同时向后移动, 当右指针到末尾时, 要删除的元素正好时左指针的下一个节点
     ListNode* removeNthFromEnd(ListNode* head, int n) {
         ListNode *l = head, *r = head;
-        for (int i = 0; i < n; ++i) {
+
+        // 右指针先向后移动n
+        for (int i = 0; i < n; i++) {
             if (r == nullptr) {
                 return head;
             }
             r = r->next;
         }
 
+        // 双指针同时向后移动
+        // 这里如果正好删除的是head, 则 r == nullptr, 不会进入这个循环
         while (r != nullptr && r->next != nullptr) {
-            r = r->next;
             l = l->next;
+            r = r->next;
         }
 
         if (r == nullptr) {
+            // 要删除的元素正好的head
             return head->next;
         } else {
             l->next = l->next->next;
