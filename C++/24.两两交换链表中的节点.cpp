ca9b326b2310231011d50d61e6ce9560af3f9029
wuyuan <2461190767@qq.com>
2023-03-29 08:23:09 +0800

    24. 两两交换链表中的节点

--- C++/24.两两交换链表中的节点.cpp
+++ C++/24.两两交换链表中的节点.cpp
@@ -1,22 +1,18 @@
-// 25. K个一组翻转链表
 
-// 给你一个链表，每k个节点一组进行翻转，请你返回翻转后的链表。
-// k是一个正整数，它的值小于或等于链表的长度。
-// 如果节点总数不是k的整数倍，那么请将最后剩余的节点保持原有顺序。
-//
-// 进阶：
-// 你可以设计一个只使用常数额外空间的算法来解决此问题吗？
-// 你不能只是单纯的改变节点内部的值，而是需要实际进行节点交换。
+// 24. 两两交换链表中的节点
+
+// 给定一个链表，两两交换其中相邻的节点，并返回交换后的链表。
+// 你不能只是单纯的改变节点内部的值，而是需要实际的进行节点交换。
 
 #include "header.h"
 
 class Solution {
 public:
-    // 24. 两两交换链表中的节点
+    // 25. K个一组翻转链表
     // 递归
-    ListNode* reverseKGroup(ListNode* head, int k) {
+    ListNode* swapPairs(ListNode* head) {
         ListNode* next_group_head = head;
-        for (int i = 0; i < k; i++) {
+        for (int i = 0; i < 2; i++) {
             if (next_group_head == nullptr) {
                 return head;
             }
@@ -26,12 +22,11 @@ public:
         // 反转当前组
         ListNode* new_head = reverse(head, next_group_head);
         // 反转剩余部分
-        head->next = reverseKGroup(next_group_head, k);
+        head->next = swapPairs(next_group_head);
 
         return new_head;
     }
 
-    // 反转链表
     ListNode* reverse(ListNode* head, ListNode* next_group_head) {
         ListNode *pre = next_group_head, *node = head, *next = head;
         while (node != next_group_head) {
