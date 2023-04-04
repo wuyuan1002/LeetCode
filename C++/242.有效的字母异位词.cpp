a68f1f97ac990442fde9ce6c2361d27be78c8528
wuyuan <2461190767@qq.com>
2023-04-04 08:16:46 +0800

    242. 有效的字母异位词

--- C++/242.有效的字母异位词.cpp
+++ C++/242.有效的字母异位词.cpp
@@ -5,23 +5,26 @@
 //
 // 注意：若s 和 t中每个字符出现的次数都相同，则称s 和 t互为字母异位词。
 
+#include <map>
 #include <string>
-#include <unordered_map>
 
 class Solution {
 public:
+    // 1. 排序两个字符串，然后比较是否相等
+    // 2. 统计两个字符串中字符的个数，所有字符出现次数相等，则是字母异位词
+
     // 使用数组记录
     bool isAnagram(std::string s, std::string t) {
         int record[26] = {0};
-        for (int i = 0; i < s.size(); ++i) {
-            ++record[s[i] - 'a'];
+        for (int i = 0; i < s.size(); i++) {
+            record[s[i] - 'a']++;
         }
-        for (int i = 0; i < t.size(); ++i) {
-            --record[t[i] - 'a'];
+        for (int j = 0; j < t.size(); j++) {
+            record[t[j] - 'a']--;
         }
 
-        for (int i = 0; i < 26; ++i) {
-            if (record[i] != 0) {
+        for (int k = 0; k < 26; k++) {
+            if (record[k] != 0) {
                 return false;
             }
         }
@@ -30,7 +33,7 @@ public:
 
     // 使用map记录
     bool isAnagram1(std::string s, std::string t) {
-        std::unordered_map<char, int> record;
+        std::map<char, int> record;
 
         for (int i = 0; i < s.size(); ++i) {
             ++record[s[i]];
