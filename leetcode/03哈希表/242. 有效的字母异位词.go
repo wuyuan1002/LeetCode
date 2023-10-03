da0f521b03e498f73397b0162218d7cf186c7149
wuyuan <2461190767@qq.com>
2023-10-03 10:14:22 +0800

    242. 有效的字母异位词
    Signed-off-by: wuyuan.ok <wuyuan.ok@bytedance.com>

---
+++ leetcode/03哈希表/242. 有效的字母异位词.go
@@ -0,0 +1,28 @@
+package leetcode
+
+// 242. 有效的字母异位词
+
+// isAnagram .
+// 1. 排序两个字符串，然后比较是否相等
+// 2. 统计两个字符串中字符的个数，所有字符出现次数相等，则是字母异位词（可以使用数组也可以使用map记录）
+func isAnagram(s string, t string) bool {
+	if len(s) != len(t) {
+		return false
+	}
+
+	record := make([]int, 26) // 使用数组记录a-z出现的次数
+
+	for _, c := range s {
+		record[c-'a']++
+	}
+	for _, c := range t {
+		record[c-'a']--
+	}
+
+	for _, n := range record {
+		if n != 0 {
+			return false
+		}
+	}
+	return true
+}
