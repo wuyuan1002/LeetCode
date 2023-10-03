e878ddaf04b36084951ec533c1a272d95428e354
wuyuan <2461190767@qq.com>
2023-10-03 15:44:58 +0800

    383. 赎金信

--- leetcode/03哈希表/242. 有效的字母异位词.go
+++ leetcode/03哈希表/242. 有效的字母异位词.go
@@ -3,6 +3,7 @@ package leetcode
 // 242. 有效的字母异位词
 
 // isAnagram .
+// 同 383. 赎金信
 // 1. 排序两个字符串，然后比较是否相等
 // 2. 统计两个字符串中字符的个数，所有字符出现次数相等，则是字母异位词（可以使用数组也可以使用map记录）
 func isAnagram(s string, t string) bool {
@@ -10,7 +11,8 @@ func isAnagram(s string, t string) bool {
 		return false
 	}
 
-	record := make([]int, 26) // 使用数组记录a-z出现的次数
+	// 使用数组记录a-z出现的次数
+	record := make([]int, 26)
 
 	for _, c := range s {
 		record[c-'a']++
---
+++ leetcode/03哈希表/383. 赎金信.go
@@ -0,0 +1,30 @@
+package leetcode
+
+// 383. 赎金信
+
+// 给定一个赎金信 (ransom) 字符串和一个杂志(magazine)字符串，
+// 判断第一个字符串 ransom 能不能由第二个字符串 magazines 里面的字符构成。
+// 如果可以构成，返回 true ；否则返回 false。
+//
+// (题目说明：为了不暴露赎金信字迹，要从杂志上搜索各个需要的字母，
+// 组成单词来表达意思。杂志字符串中的每个字符只能在赎金信字符串中使用一次。)
+
+// canConstruct .
+// 同 242. 有效的字母异位词
+// 使用map存第一个字符串字符出现的次数
+func canConstruct(ransomNote string, magazine string) bool {
+	// 使用数组记录magazine每个字符出现的次数
+	record := make([]int, 26)
+	for _, c := range magazine {
+		record[c-'a']++
+	}
+
+	// 遍历ransomNote判断, 能否由magazine的字符构成
+	for _, c := range ransomNote {
+		record[c-'a']--
+		if record[c-'a'] < 0 {
+			return false
+		}
+	}
+	return true
+}
