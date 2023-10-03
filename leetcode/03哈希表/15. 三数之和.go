b99515a2141d49d0bcbd6e818ba25b410f54efea
wuyuan <2461190767@qq.com>
2023-10-03 11:55:29 +0800

    15. 三数之和
    Signed-off-by: wuyuan.ok <wuyuan.ok@bytedance.com>

---
+++ leetcode/03哈希表/15. 三数之和.go
@@ -0,0 +1,80 @@
+package leetcode
+
+// 15. 三数之和
+
+// 给你一个包含 n 个整数的数组nums，判断nums中是否存在三个元素 a，b，c ，
+// 使得a + b + c = 0 ？请你找出所有和为 0 且不重复的三元组。
+//
+// 注意：答案中不可以包含重复的三元组。
+
+// threeSum .
+// 先固定一个值，接下来就是在剩余数字中寻找两数之和，但是需要保证结果不重复，
+// 如果寻找两数之和时使用map的话，结果会重复；因此我们先将数组排序，在寻找两数之和时使用双指针从两边移动来找
+// 排序 + 双指针
+func threeSum(nums []int) [][]int {
+	if nums == nil || len(nums) < 3 {
+		return nil
+	}
+
+	result := make([][]int, 0)
+
+	// 排序
+	quickSort(nums, 0, len(nums)-1)
+
+	for i, num := range nums {
+		if num > 0 {
+			// 剪枝 -- 若i指向大于0的数，则直接退出，因为数组已排序，i后面的数也肯定都是正数
+			break
+		} else if i > 0 && num == nums[i-1] {
+			// 剪枝 -- 跳过重复元素
+			continue
+		}
+
+		l, r := i+1, len(nums)-1
+		for l < r {
+			sum := num + nums[l] + nums[r]
+			if sum > 0 {
+				r--
+			} else if sum < 0 {
+				l++
+			} else {
+				// 记录结果
+				result = append(result, []int{num, nums[l], nums[r]})
+
+				// 剪枝 -- 跳过重复元素
+				for l++; l < r && nums[l] == nums[l-1]; l++ {
+				}
+				for r--; l < r && nums[r] == nums[r+1]; r-- {
+				}
+			}
+		}
+	}
+
+	return result
+}
+
+// quickSort 快排
+func quickSort(nums []int, left, right int) {
+	if nums == nil || len(nums) == 0 || left >= right {
+		return
+	}
+
+	l, r := left, right
+	temp := nums[left]
+
+	for l < r {
+		for l < r && nums[r] >= temp {
+			r--
+		}
+		for l < r && nums[l] <= temp {
+			l++
+		}
+		if l < r {
+			nums[l], nums[r] = nums[r], nums[l]
+		}
+	}
+
+	nums[left], nums[l] = nums[l], nums[left]
+	quickSort(nums, left, l-1)
+	quickSort(nums, r+1, right)
+}
