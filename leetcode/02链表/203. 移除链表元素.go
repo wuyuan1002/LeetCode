6ccb36c61b82bde927a0310de208fc70e0f98da2
wuyuan <2461190767@qq.com>
2023-10-03 08:21:19 +0800

    203. 移除链表元素
    Signed-off-by: wuyuan.ok <wuyuan.ok@bytedance.com>

---
+++ leetcode/02链表/203. 移除链表元素.go
@@ -0,0 +1,28 @@
+package leetcode
+
+// 203. 移除链表元素
+
+// 给你一个链表的头节点 head 和一个整数 val ，
+// 请你删除链表中所有满足 Node.val == val 的节点，并返回 新的头节点 。
+
+// removeElements .
+func removeElements(head *ListNode, val int) *ListNode {
+	if head == nil {
+		return nil
+	}
+
+	dummy := &ListNode{Next: head} // 虚拟头节点
+	for node := dummy; node != nil && node.Next != nil; {
+		if node.Next.Val == val {
+			node.Next = node.Next.Next
+		} else {
+			node = node.Next
+		}
+	}
+	return dummy.Next
+}
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
