5f6a199af4f4be526aaf1d24ca5ee3853ecf4327
wuyuan <2461190767@qq.com>
2023-10-20 13:34:48 +0800

    55. 跳跃游戏
    Signed-off-by: wuyuan.ok <wuyuan.ok@bytedance.com>

--- leetcode/08贪心/53. 最大子数组和.go
+++ leetcode/08贪心/53. 最大子数组和.go
@@ -16,7 +16,7 @@ func maxSubArray(nums []int) int {
 		return nums[0]
 	}
 
-	currentSum, maxSum := nums[0], nums[0] // 当前的最大连续和、最大子数组和
+	currentSum, maxSum := nums[0], nums[0] // 以当前数字结尾的最大子数组和、全局最大子数组和
 	for i := 1; i < len(nums); i++ {
 		// 更新当前最大的连续和
 		currentSum = max(currentSum+nums[i], nums[i])
---
+++ leetcode/08贪心/55. 跳跃游戏.go
@@ -0,0 +1,28 @@
+package main
+
+// 55. 跳跃游戏
+
+// 给定一个非负整数数组 nums ，你最初位于数组的 第一个下标 。
+// 数组中的每个元素代表你在该位置可以跳跃的最大长度。
+// 判断你是否能够到达最后一个下标。
+
+// canJump .
+// 同 leetcode 45. 跳跃游戏 II
+// 从某一位置起跳，能跳到的最远位置之前的位置都是可以跳到的。
+// 所以，遍历每一个位置，不断更新最远位置，最远位置之前的都是可以跳到的
+func canJump(nums []int) bool {
+	farthest := 0 // 能跳到的最远距离
+
+	// 不断向前移动
+	for i := 0; i < len(nums); i++ {
+		// 若当前位置已经超过能跳到的最远位置，说明跳不到数组最后
+		if i > farthest {
+			return false
+		}
+
+		// 更新能跳到的最远位置
+		farthest = max(i+nums[i], farthest)
+	}
+
+	return true
+}
