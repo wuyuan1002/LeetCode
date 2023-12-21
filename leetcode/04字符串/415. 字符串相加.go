ae32ee64d2101c23feb7f7251be59e5529f2d764
wuyuan <2461190767@qq.com>
2023-12-21 13:50:09 +0800

    67. 二进制求和
    Signed-off-by: wuyuan.ok <wuyuan.ok@bytedance.com>

--- leetcode/04字符串/415. 字符串相加.go
+++ leetcode/04字符串/415. 字符串相加.go
@@ -7,6 +7,7 @@ package main
 // 你不能使用任何內建的用于处理大整数的库（比如 BigInteger）， 也不能直接将输入的字符串转换为整数形式。
 
 // addStrings .
+// 同 leetcode 43. 字符串相乘、67. 二进制求和
 // 两指针分别从两字符串尾部进行相加操作，同时使用一个变量记录进位，将进位相加到下一位的计算中，
 // 使用一个切片保存每一位的得数，将每一位的计算结果添加到切片末尾，最终计算完后反转整个切片即为最终结果
 func addStrings(num1 string, num2 string) string {
@@ -14,6 +15,7 @@ func addStrings(num1 string, num2 string) string {
 	carry := 0                       // 进位
 	i, j := len(num1)-1, len(num2)-1 // 两个指针分别指向两数末尾
 
+	// 不断计算每一位的得数和进位，并将每一位的得数添加到总结果尾部
 	for i >= 0 || j >= 0 || carry > 0 {
 		// 获取i、j位置对应的的数字
 		n1, n2 := 0, 0
@@ -26,7 +28,7 @@ func addStrings(num1 string, num2 string) string {
 
 		// 计算当前位得数和进位
 		sum := n1 + n2 + carry
-		result = append(result, byte((sum%10)+'0'))
+		result = append(result, byte(sum%10)+'0')
 		carry = sum / 10
 
 		// 同时向前移动两指针
---
+++ leetcode/04字符串/67. 二进制求和.go
@@ -0,0 +1,47 @@
+package main
+
+// 67. 二进制求和
+
+// 给你两个二进制字符串 a 和 b ，以二进制字符串的形式返回它们的和。
+
+// addBinary .
+// 同 leetcode 415. 字符串相加、43. 字符串相乘
+// 此题与 415. 字符串相加 完全一致，只是415是十进制，此题是二进制（在求当前位得数和进位时使用的是2而不是10）
+func addBinary(a string, b string) string {
+	result := make([]byte, 0)  // 总结果
+	carry := 0                 // 进位
+	i, j := len(a)-1, len(b)-1 // 两个指针分别指向两数末尾
+
+	// 不断计算每一位的得数和进位，并将每一位的得数添加到总结果尾部
+	for i >= 0 || j >= 0 || carry > 0 {
+		// 获取i、j位置对应的的数字
+		n1, n2 := 0, 0
+		if i >= 0 {
+			n1 = int(a[i] - '0')
+		}
+		if j >= 0 {
+			n2 = int(b[j] - '0')
+		}
+
+		// 计算当前位得数和进位
+		sum := n1 + n2 + carry
+		result = append(result, byte(sum%2)+'0')
+		carry = sum / 2
+
+		// 同时向前移动两指针
+		i--
+		j--
+	}
+
+	// 反转总结果切片
+	reverse67(result)
+
+	return string(result)
+}
+
+// reverse67 .
+func reverse67(nums []byte) {
+	for l, r := 0, len(nums)-1; l < r; l, r = l+1, r-1 {
+		nums[l], nums[r] = nums[r], nums[l]
+	}
+}
