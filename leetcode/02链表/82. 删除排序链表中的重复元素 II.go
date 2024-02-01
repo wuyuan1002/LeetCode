99b7773882cfe6b16e6990d37fed5de91ed7cd78
wuyuan <2461190767@qq.com>
2024-02-01 13:33:25 +0800

    82. 删除排序链表中的重复元素 II
    Signed-off-by: wuyuan.ok <wuyuan.ok@bytedance.com>

---
+++ leetcode/02链表/82. 删除排序链表中的重复元素 II.go
@@ -0,0 +1,30 @@
+package main
+
+// 82. 删除排序链表中的重复元素 II
+
+// 给定一个已排序的链表的头 head ， 删除原始链表中所有重复数字的节点，只留下不同的数字 。返回 已排序的链表 。
+
+// deleteDuplicates .
+func deleteDuplicates(head *ListNode) *ListNode {
+
+	// 虚拟头节点
+	dummy := &ListNode{Next: head}
+
+	// node初始为虚拟头节点，不断判断node的后两个节点值是否为重复数字，若不重复则将node向后移动一位然后继续判断其后两个值是否为重复数字，
+	// 若node的后两个节点为重复数字，则需要将所有重复的数字从链表中移除，也就是将node的下一位指向移除当前重复数字后的首个节点
+	node := dummy
+	for node.Next != nil && node.Next.Next != nil {
+		if node.Next.Val == node.Next.Next.Val {
+			// node的后两个节点为重复数字，需要将所有重复的数字移除 -- 不断判断node.next是否还是重复数字并向后移动node.next
+			val := node.Next.Val
+			for node.Next != nil && node.Next.Val == val {
+				node.Next = node.Next.Next
+			}
+		} else {
+			// node的后两个节点不是重复数字，将node向后移动一位，继续判断下一位的后两个数字是否重复
+			node = node.Next
+		}
+	}
+
+	return dummy.Next
+}
