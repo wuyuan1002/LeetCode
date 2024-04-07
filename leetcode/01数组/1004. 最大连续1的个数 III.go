afcde662d90c2c850c45d7e2641e9c82f8251ec7
wuyuan <2461190767@qq.com>
2024-04-07 19:46:12 +0800

    update
    Signed-off-by: wuyuan.ok <wuyuan.ok@bytedance.com>

--- leetcode/01数组/1004. 最大连续1的个数 III.go
+++ leetcode/01数组/1004. 最大连续1的个数 III.go
@@ -5,6 +5,7 @@ package main
 // 给定一个二进制数组 nums 和一个整数 k，如果可以翻转最多 k 个 0 ，则返回 数组中连续 1 的最大个数 。
 
 // longestOnes .
+// 同 leetcode 424. 替换后的最长重复字符
 // 滑动窗口 -- 求窗口中0的个数小于k的最大窗口长度
 func longestOnes(nums []int, k int) int {
 
