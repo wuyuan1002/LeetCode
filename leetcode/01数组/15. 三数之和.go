2ef4226bb7104590c67d1e6f6f6affa6d3e3cbf1
wuyuan <2461190767@qq.com>
2024-06-29 10:27:58 +0800

    15. 三数之和
    Signed-off-by: wuyuan.ok <wuyuan.ok@bytedance.com>

--- leetcode/01数组/15. 三数之和.go
+++ leetcode/01数组/15. 三数之和.go
@@ -1,5 +1,7 @@
 package main
 
+import "sort"
+
 // 15. 三数之和
 
 // 给你一个包含 n 个整数的数组nums，判断nums中是否存在三个元素 a，b，c ，
@@ -8,20 +10,22 @@ package main
 // 注意：答案中不可以包含重复的三元组。
 
 // threeSum .
+// 排序 + 双指针
+//
 // 先固定一个值，接下来就是在剩余数字中寻找两数之和，但是需要保证结果不重复，
 // 如果寻找两数之和时使用map的话，结果会重复；因此我们先将数组排序，在寻找两数之和时使用双指针从两边移动来找
-// 排序 + 双指针
 func threeSum(nums []int) [][]int {
 	if nums == nil || len(nums) < 3 {
 		return nil
 	}
 
-	quickSort(nums, 0, len(nums)-1)
+	// 先将数组从小到大排序
+	sort.Ints(nums)
 
 	result := make([][]int, 0)
 	for i := 0; i < len(nums); i++ {
 		if nums[i] > 0 {
-			// 剪枝 -- 若i指向大于0的数，则直接退出，因为数组已排序，i后面的数也肯定都是正数
+			// 剪枝 -- 若i指向大于0的数，则直接退出，因为数组已排序，i后面的数也肯定都大于0
 			break
 		} else if i > 0 && nums[i] == nums[i-1] {
 			// 剪枝 -- 跳过重复元素
@@ -32,18 +36,18 @@ func threeSum(nums []int) [][]int {
 		for l < r {
 			sum := nums[i] + nums[l] + nums[r]
 			if sum > 0 {
-				// 移动到下一个不相同数字位置
+				// 右指针移动到下一个不相同数字位置
 				for r--; l < r && nums[r] == nums[r+1]; r-- {
 				}
 			} else if sum < 0 {
-				// 移动到下一个不相同数字位置
+				// 左指针移动到下一个不相同数字位置
 				for l++; l < r && nums[l] == nums[l-1]; l++ {
 				}
 			} else {
 				// 记录结果
 				result = append(result, []int{nums[i], nums[l], nums[r]})
 
-				// 剪枝 -- 跳过重复元素
+				// 剪枝 -- 跳过重复元素，左右指针都移动到下一个不同数字位置
 				for l++; l < r && nums[l] == nums[l-1]; l++ {
 				}
 				for r--; l < r && nums[r] == nums[r+1]; r-- {
