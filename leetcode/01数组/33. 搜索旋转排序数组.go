f3b7b1ed485963afd625ee5c640633497ffdf322
wuyuan <2461190767@qq.com>
2024-07-01 19:43:23 +0800

    33. 搜索旋转排序数组
    Signed-off-by: wuyuan.ok <wuyuan.ok@bytedance.com>

--- leetcode/01数组/33. 搜索旋转排序数组.go
+++ leetcode/01数组/33. 搜索旋转排序数组.go
@@ -9,12 +9,17 @@ package main
 // 给你旋转后的数组nums和一个整数target，如果 nums 中存在这个目标值target，则返回它的下标，否则返回-1。
 
 // search33 .
-// 同 leetcode 81. 搜索旋转排序数组 II
+// leetcode 81. 搜索旋转排序数组 II
+//
+// 二分查找
+// 虽然无法通过nums[mid]与target的大小关系确定target在mid的左侧还是右侧，
+// 但是可以通过nums[l]、nums[mid]、nums[r]三者来确定好target的方位，然后进行左右指针的收缩
 func search33(nums []int, target int) int {
 	l, r := 0, len(nums)-1
 	for l <= r {
 		mid := l + (r-l)/2
-		if nums[mid] == target {
+
+		if nums[mid] == target { // 若mid等于目标值则直接返回
 			return mid
 		}
 
