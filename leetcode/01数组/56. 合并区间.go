73a89fcfba2812541560bed7c9f6cc1e410ec25b
wuyuan <2461190767@qq.com>
2024-07-10 17:40:34 +0800

    56. 合并区间
    Signed-off-by: wuyuan.ok <wuyuan.ok@bytedance.com>

--- leetcode/01数组/56. 合并区间.go
+++ leetcode/01数组/56. 合并区间.go
@@ -8,7 +8,8 @@ import "sort"
 // 请你合并所有重叠的区间，并返回一个不重叠的区间数组，该数组需恰好覆盖输入中的所有区间。
 
 // merge .
-// 将每个区间按照左端点升序排序，然后进行区间合并，若后一个区间的左端点大于前一个区间的右端点，说明两个区间没有交集，否则说明有交集
+// 将每个区间按照左端点升序排序，然后进行区间合并，
+// 若后一个区间的左端点大于前一个区间的右端点，说明两个区间没有交集，否则说明有交集
 func merge(intervals [][]int) [][]int {
 	// 按照左端点升序排序
 	sort.Slice(intervals, func(i, j int) bool { return intervals[i][0] < intervals[j][0] })
@@ -16,6 +17,7 @@ func merge(intervals [][]int) [][]int {
 	result := make([][]int, 0) // 总结果
 	current := intervals[0]    // 当前合并区间
 
+	// 从第2个区间开始进行区间合并
 	for i := 1; i < len(intervals); i++ {
 		interval := intervals[i] // 当前区间
 
@@ -23,11 +25,10 @@ func merge(intervals [][]int) [][]int {
 			// 当前区间与当前合并区间有交集 -- 更新当前合并区间的最大右边界
 			current[1] = max(interval[1], current[1])
 		} else {
-			// 当前区间与当前合并区间没有交集 -- 将合并总区间记录到结果集，当前区间为下一个合并区间
-			temp := make([]int, len(current))
-			copy(temp, current)
-			result = append(result, temp)
+			// 当前区间与当前合并区间没有交集 -- 将当前合并的总区间记录到结果集
+			result = append(result, []int{current[0], current[1]})
 
+			// 将当前区间置为下一个合并总区间，继续合并后续区间
 			current = interval
 		}
 	}
