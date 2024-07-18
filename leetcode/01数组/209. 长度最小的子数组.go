bad9db7cae4010471670dda3f24f82b1f7a34ea7
wuyuan <2461190767@qq.com>
2024-07-18 16:08:42 +0800

    209. 长度最小的子数组
    Signed-off-by: wuyuan.ok <wuyuan.ok@bytedance.com>

--- leetcode/01数组/209. 长度最小的子数组.go
+++ leetcode/01数组/209. 长度最小的子数组.go
@@ -10,25 +10,28 @@ import "math"
 // 并返回其长度。如果不存在符合条件的子数组，返回 0 。
 
 // minSubArrayLen .
-// 同 leetcode 713. 乘积小于 K 的子数组
+// leetcode 713. 乘积小于 K 的子数组
 //
-// 双指针, 滑动窗口
-// 右指针不断向前移动把数字加到sum, 当发现 sum >= target 时, 开始向前移动左指针, 并不断记录窗口大小
+// 滑动窗口
+// 右指针不断向前移动把数字加到sum, 当发现 sum >= target 时, 开始向前移动左指针缩小窗口, 并不断记录窗口大小
 func minSubArrayLen(target int, nums []int) int {
 	if nums == nil || len(nums) == 0 {
 		return 0
 	}
 
-	sum, minLen := 0, math.MaxInt // 当前和、子数组最小长度
+	// 当前和、子数组最小长度
+	sum, minLen := 0, math.MaxInt
+
 	// 定义快慢指针, 不断向前移动右指针
 	for l, r := 0, 0; r < len(nums); r++ {
-		// 将右指针的值加到当前和上, 当发现sum >= target时开始向前移动左指针并记录最小子数组长度
+		// 将右指针的值加到当前和上, 当发现 sum >= target 时开始向前移动左指针并记录最小子数组长度
 		for sum += nums[r]; sum >= target && l <= r; l++ {
 			minLen = min(minLen, r-l+1)
 			sum -= nums[l]
 		}
 	}
 
+	// 若最小长度为math.MaxInt说明整个数组的数字加起来都没有target大，不存在满足条件的左指针
 	if minLen == math.MaxInt {
 		return 0
 	}
