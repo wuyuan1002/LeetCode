8398a440feacc86d38dfc8d2190ecde560a0274b
wuyuan <2461190767@qq.com>
2024-07-24 11:21:39 +0800

    414. 第三大的数
    Signed-off-by: wuyuan.ok <wuyuan.ok@bytedance.com>

--- leetcode/01数组/414. 第三大的数.go
+++ leetcode/01数组/414. 第三大的数.go
@@ -7,19 +7,19 @@ import "math"
 // 给你一个非空数组，返回此数组中 第三大的数 。如果不存在，则返回数组中最大的数。
 
 // thirdMax .
-// 1. 使用三个变量分别记录最大、第二大和第三大的数子，一次遍历数组不断更新三个值，遍历结束后返回第三大的数字
-// 2. 快排partition -- 快排过程中若发现当前归位了第三大的数字则直接返回不继续排序
+// 使用三个变量分别记录最大、第二大和第三大的数字，一次遍历数组不断更新三个值，遍历结束后返回第三大的数字
 func thirdMax(nums []int) int {
 	a, b, c := math.MinInt64, math.MinInt64, math.MinInt64
 	for _, num := range nums {
-		if num > a {
+		if num > a { // 比最大的数大
 			a, b, c = num, a, b
-		} else if num > b && num < a {
+		} else if num > b && num < a { // 比第二大的数大
 			b, c = num, b
-		} else if num > c && num < b {
+		} else if num > c && num < b { // 比第三大的数大
 			c = num
 		}
 	}
+
 	if c == math.MinInt64 {
 		return a
 	}
