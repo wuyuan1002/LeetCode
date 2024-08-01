e27b94516d1b25d303f105e2cc751daac1fd0e89
wuyuan <2461190767@qq.com>
2024-08-01 10:57:42 +0800

    658. 找到 K 个最接近的元素
    Signed-off-by: wuyuan.ok <wuyuan.ok@bytedance.com>

--- leetcode/01数组/658. 找到 K 个最接近的元素.go
+++ leetcode/01数组/658. 找到 K 个最接近的元素.go
@@ -4,27 +4,34 @@ import "sort"
 
 // 658. 找到 K 个最接近的元素
 
-// 给定一个 排序好 的数组 arr ，两个整数 k 和 x ，从数组中找到最靠近 x（两数之差最小）的 k 个数。返回的结果必须要是按升序排好的。
+// 给定一个 排序好 的数组 arr ，两个整数 k 和 x ，从数组中找到最靠近 x（两数之差最小）的 k 个数。
+// 返回的结果必须要是按升序排好的。
 //
 // 整数 a 比整数 b 更接近 x 需要满足：
 // |a - x| < |b - x| 或者
 // |a - x| == |b - x| 且 a < b
 
-// findClosestElements .
+// findClosestElements1 .
 // 若差值不同，则差值越小越靠近，若差值相同则数字越小越靠近
-// 1. 将数组按照「更靠近」进行重新排序，排序后数组的前k个数字就是答案 -- 由于数组本身就是从小到大排序好的，所以若差值相同时，本身在前的更靠近，因此使用稳定排序即可在差值相同时不破坏原有顺序
-func findClosestElements(arr []int, k int, x int) []int {
-	// 稳定排序，在差值绝对值相同的情况下，保证更小的数排在前面
+//
+// 将数组按照「更靠近」进行重新排序，排序后数组的前k个数字就是答案
+// 由于数组本身就是从小到大排序好的，所以若差值相同时，本身在前的更靠近，
+// 因此使用稳定排序即可在差值相同时不破坏原有顺序
+func findClosestElements1(arr []int, k int, x int) []int {
+	// 稳定排序，在差值绝对值相同的情况下，保证更小的数排在前面（因为原数组本身已经是排好序的）
 	sort.SliceStable(arr, func(i, j int) bool { return abs(arr[i]-x) < abs(arr[j]-x) })
+
+	// 数组前k个数数字就是最靠近x的k个数字，对这k个数字进行排序后返回
 	arr = arr[:k]
 	sort.Ints(arr)
 	return arr
 }
 
-// findClosestElements .
+// findClosestElements2 .
 // 若差值不同，则差值越小越靠近，若差值相同则数字越小越靠近
-// 2. 先使用二分查找找到数组中最接近x的数字，然后使用双指针向两边进行拓展
-func findClosestElements1(arr []int, k int, x int) []int {
+//
+// 先使用二分查找找到数组中最接近x的数字，然后使用双指针向两边进行拓展，最终的区间为(left, right)
+func findClosestElements2(arr []int, k int, x int) []int {
 	right := sort.SearchInts(arr, x) // 找到第一个大于等于x的数字下标
 	left := right - 1                // 最后一个小于x的数字下标
 
@@ -33,7 +40,7 @@ func findClosestElements1(arr []int, k int, x int) []int {
 		if left < 0 {
 			right++
 		} else if right >= len(arr) || x-arr[left] <= arr[right]-x {
-			// 若左右都可扩展，且左右差值相同时，优先想左侧扩展
+			// 若左右都可扩展，且左右差值相同时，优先向左侧扩展
 			left--
 		} else {
 			right++
