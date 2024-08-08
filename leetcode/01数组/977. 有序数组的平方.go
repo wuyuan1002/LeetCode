796d900fce586a254345431f9c5e21f5176ef9f7
wuyuan <2461190767@qq.com>
2024-08-08 11:39:17 +0800

    977. 有序数组的平方
    Signed-off-by: wuyuan.ok <wuyuan.ok@bytedance.com>

--- leetcode/01数组/977. 有序数组的平方.go
+++ leetcode/01数组/977. 有序数组的平方.go
@@ -5,16 +5,21 @@ package main
 // 给你一个按 非递减顺序 排序的整数数组 nums，返回 每个数字的平方 组成的新数组，
 // 要求也按 非递减顺序 排序。
 
-// sortedSquares 双指针
-// 负数的平方可能成为最大值，因此，新数组的最大值出现在旧数组两端
+// sortedSquares .
+// 双指针
+// 负数的平方也可能成为最大值，所以新数组的最大值出现在旧数组的两端数字的平方
+// 使用双指针从首尾分别选择两个数字，根据绝对值判断两个数字谁在前谁在后
 func sortedSquares(nums []int) []int {
 	if nums == nil || len(nums) == 0 {
 		return nil
 	}
 
-	result := make([]int, len(nums))
-	l, r := 0, len(nums)-1
+	result := make([]int, len(nums)) // 总结果
+	l, r := 0, len(nums)-1           // 左右指针
+
+	// 倒序遍历结果数组，根据旧数组首尾数字的绝对值由大到小填充结果数组
 	for i := len(result) - 1; l <= r; i-- {
+		// 若左指针的绝对值小于右指针，则说明右指针指向数字的平方更大，填充右指针数字平方进结果数组，否则填充左指针数字平方进结果数组
 		if abs(nums[l]) < abs(nums[r]) {
 			result[i] = nums[r] * nums[r]
 			r--
@@ -23,6 +28,7 @@ func sortedSquares(nums []int) []int {
 			l++
 		}
 	}
+
 	return result
 }
 
