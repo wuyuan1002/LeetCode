9ad5eed4efaec404eb73f6374cd28b73692c8999
wuyuan <2461190767@qq.com>
2024-08-14 16:38:24 +0800

    21. 合并两个有序链表
    Signed-off-by: wuyuan.ok <wuyuan.ok@bytedance.com>

--- leetcode/02链表/21. 合并两个有序链表.go
+++ leetcode/02链表/21. 合并两个有序链表.go
@@ -5,7 +5,7 @@ package main
 // 将两个升序链表合并为一个新的 升序 链表并返回。新链表是通过拼接给定的两个链表的所有节点组成的。
 
 // mergeTwoLists .
-// 同 leetcode 23. 合并K个升序链表
+// leetcode 23. 合并K个升序链表
 func mergeTwoLists(list1 *ListNode, list2 *ListNode) *ListNode {
 	if list1 == nil {
 		return list2
