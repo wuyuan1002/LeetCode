6dbe2aaa9218a359ab61c5fb7b28edd6aa693b2a
wuyuan <2461190767@qq.com>
2024-08-16 10:48:56 +0800

    25. K个一组翻转链表
    Signed-off-by: wuyuan.ok <wuyuan.ok@bytedance.com>

--- leetcode/02链表/25. K个一组翻转链表.go
+++ leetcode/02链表/25. K个一组翻转链表.go
@@ -11,7 +11,10 @@ package main
 // 你不能只是单纯的改变节点内部的值，而是需要实际进行节点交换。
 
 // reverseKGroup .
-// 同 leetcode 24. 两两交换链表中的节点
+// leetcode 24. 两两交换链表中的节点
+//
+// 按照k个节点进行分组，对每一组进行反转链表，
+// 然后将反转后当前组的尾节点指向反转后下一组的头节点，最终返回当前组的头节点
 func reverseKGroup(head *ListNode, k int) *ListNode {
 	if head == nil {
 		return nil
@@ -26,15 +29,15 @@ func reverseKGroup(head *ListNode, k int) *ListNode {
 		nextGroupHead = nextGroupHead.Next
 	}
 
-	// 翻转当前组
+	// 反转当前组
 	newHead := reverse(head, nextGroupHead)
-	// 翻转之后的节点
+	// 翻转之后的节点，并将反转后当前组的尾节点指向反转后下一组的头节点
 	head.Next = reverseKGroup(nextGroupHead, k)
 
 	return newHead
 }
 
-// reverse 翻转两个节点之间的节点 [)
+// reverse 反转链表 -- 反转两个节点之间的节点 [)
 func reverse(head, nextGroupHead *ListNode) *ListNode {
 	pre, node, next := (*ListNode)(nil), head, head
 	for node != nextGroupHead {
