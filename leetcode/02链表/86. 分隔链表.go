38fee99d1b84f9ad135269a092bcd348527c33e9
wuyuan <2461190767@qq.com>
2024-08-20 11:05:59 +0800

    86. 分隔链表
    Signed-off-by: wuyuan.ok <wuyuan.ok@bytedance.com>

--- leetcode/02链表/86. 分隔链表.go
+++ leetcode/02链表/86. 分隔链表.go
@@ -2,7 +2,8 @@ package main
 
 // 86. 分隔链表
 
-// 给你一个链表的头节点 head 和一个特定值 x ，请你对链表进行分隔，使得所有 小于 x 的节点都出现在 大于或等于 x 的节点之前。
+// 给你一个链表的头节点 head 和一个特定值 x ，请你对链表进行分隔，
+// 使得所有 小于 x 的节点都出现在 大于或等于 x 的节点之前。
 //
 // 你应当 保留 两个分区中每个节点的初始相对位置。
 
@@ -25,8 +26,8 @@ func partition(head *ListNode, x int) *ListNode {
 	}
 
 	// 将两个链表首尾相连 -- 大的连到小的后面
-	big.Next = nil
 	small.Next = bigHead.Next
+	big.Next = nil
 
 	return smallHead.Next
 }
