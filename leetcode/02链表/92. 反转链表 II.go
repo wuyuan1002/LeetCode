0d1ae7d0c215c4a8f06a9dd258ff371881d1100b
wuyuan <2461190767@qq.com>
2024-08-22 10:25:41 +0800

    92. 反转链表 II
    Signed-off-by: wuyuan.ok <wuyuan.ok@bytedance.com>

--- leetcode/02链表/92. 反转链表 II.go
+++ leetcode/02链表/92. 反转链表 II.go
@@ -2,11 +2,13 @@ package main
 
 // 92. 反转链表 II
 
-// 给你单链表的头指针 head 和两个整数 left 和 right ，其中 left <= right 。请你反转从位置 left 到位置 right 的链表节点，返回 反转后的链表 。
+// 给你单链表的头指针 head 和两个整数 left 和 right ，其中 left <= right 。
+// 请你反转从位置 left 到位置 right 的链表节点，返回 反转后的链表 。
 
 // reverseBetween .
-// 1. 使用 leetcode 206. 反转链表，先遍历链表找到需要反转的节点范围，然后反转链表 -- 共需要遍历链表两次
-// 2. 寻找到左节点后继续向后遍历寻找右节点，过程中完成反转操作 -- 总共需要遍历一次
+// leetcode 206. 反转链表
+//
+// 先遍历链表寻找到左节点（过程中不进行反转操作），然后继续向后遍历寻找右节点（过程中进行反转操作）
 func reverseBetween(head *ListNode, left int, right int) *ListNode {
 	// 构建一个虚拟头节点
 	dummy := &ListNode{Next: head}
@@ -17,7 +19,7 @@ func reverseBetween(head *ListNode, left int, right int) *ListNode {
 		pre = pre.Next
 	}
 
-	// 继续向后遍历寻找右节点，过程中完成反转操作 -- 不断将右侧节点移动到pre的next
+	// 继续向后遍历当前节点，直到找到右节点，过程中完成反转操作 -- leetcode 206. 反转链表
 	node, next := pre.Next, pre.Next
 	for i := 0; i < right-left; i++ {
 		next = node.Next
