2d0ab4764e587a467020482ea7dbd2b961f98641
wuyuan <2461190767@qq.com>
2024-08-28 10:15:29 +0800

    234. 回文链表
    Signed-off-by: wuyuan.ok <wuyuan.ok@bytedance.com>

--- leetcode/02链表/234. 回文链表.go
+++ leetcode/02链表/234. 回文链表.go
@@ -9,42 +9,27 @@ package main
 // 2.将链表遍历放入数组中，之后使用双指针从头尾遍历比较
 // 3.先使用快慢指针找到链表中间的节点，之后将前半部分或后半部分翻转，之后使用双指针遍历前后两个部分进行比较
 func isPalindrome(head *ListNode) bool {
-	// 1. 快慢指针寻找链表中点 -- 偶数找到的是前一个
+	// 快慢指针寻找链表中点 -- 偶数找到的是前一个
 	slow, fast := head, head
 	for fast.Next != nil && fast.Next.Next != nil {
 		slow = slow.Next
 		fast = fast.Next.Next
 	}
 
-	// 2. 反转链表后半段
-	tail := reversList234(slow.Next)
+	// 反转链表后半段
+	tail := reverseList(slow.Next)
 
-	// 3. 挨个遍历未反转的前半段和反转后的后半段各个节点值是否相同
+	// 挨个遍历未反转的前半段和反转后的后半段各个节点值是否相同
 	result := true
-	p, q := head, tail
-	for result && q != nil {
+	for p, q := head, tail; result && q != nil; p, q = p.Next, q.Next {
 		if p.Val != q.Val {
 			result = false
 		}
-		p = p.Next
-		q = q.Next
 	}
 
-	// 4. 恢复链表
-	slow.Next = reversList234(tail)
+	// 恢复链表
+	slow.Next = reverseList(tail)
 
-	// 5. 返回结果
+	// 返回结果
 	return result
-}
-
-// reversList234 反转链表
-func reversList234(head *ListNode) *ListNode {
-	pre, node, next := (*ListNode)(nil), head, head
-	for node != nil {
-		next = node.Next
-		node.Next = pre
-		pre = node
-		node = next
-	}
-	return pre
 }
