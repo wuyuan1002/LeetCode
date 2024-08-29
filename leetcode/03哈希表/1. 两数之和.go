61a8d16eee29493ab10e36dfca0756f9cc300bf2
wuyuan <2461190767@qq.com>
2024-08-29 10:13:13 +0800

    1. 两数之和
    Signed-off-by: wuyuan.ok <wuyuan.ok@bytedance.com>

--- leetcode/03哈希表/1. 两数之和.go
+++ leetcode/03哈希表/1. 两数之和.go
@@ -2,17 +2,18 @@ package main
 
 // 1. 两数之和
 
-// 给定一个整数数组 nums和一个整数目标值 target，
+// 给定一个整数数组 nums 和一个整数目标值 target，
 // 请你在该数组中找出和为目标值 target 的那两个整数，并返回它们的数组下标。
 //
 // 你可以假设每种输入只会对应一个答案。但是，数组中同一个元素在答案里不能重复出现。
 // 你可以按任意顺序返回答案。
 
 // twoSum .
-// 类似 leetcode 202. 快乐数
-// 在查找的过程中将数字存入map中，这样，下次使用时可以直接获取
+// leetcode 202. 快乐数
+//
+// 在查找的过程中将数字存入map中，方便快速判断某数字是否出现过
 func twoSum(nums []int, target int) []int {
-	// 使用一个map存已经出现过的数字 -- k: 数值, v: 下标
+	// 使用一个map存已经出现过的数字 -- k: 数字, v: 下标
 	hash := make(map[int]int)
 
 	for i, num := range nums {
