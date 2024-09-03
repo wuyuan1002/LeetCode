2dea0f7e09c9795eb23e468a9f23f991a30b77ce
wuyuan <2461190767@qq.com>
2024-09-03 10:26:49 +0800

    242. 有效的字母异位词
    Signed-off-by: wuyuan.ok <wuyuan.ok@bytedance.com>

--- leetcode/03哈希表/242. 有效的字母异位词.go
+++ leetcode/03哈希表/242. 有效的字母异位词.go
@@ -3,9 +3,10 @@ package main
 // 242. 有效的字母异位词
 
 // isAnagram .
-// 同 383. 赎金信
+// leetcode 383. 赎金信
+//
 // 1. 排序两个字符串，然后比较是否相等
-// 2. 统计两个字符串中字符的个数，所有字符出现次数相等，则是字母异位词（可以使用数组也可以使用map记录）
+// 2. 统计两个字符串中字符的个数，所有字符出现次数相等，则是字母异位词（可以使用数组也可以使用map进行记录）
 func isAnagram(s string, t string) bool {
 	if len(s) != len(t) {
 		return false
@@ -26,5 +27,6 @@ func isAnagram(s string, t string) bool {
 			return false
 		}
 	}
+
 	return true
 }
