e724187c7e03efcf9efa2707ecd8bb5d6dd38919
wuyuan <2461190767@qq.com>
2024-09-03 10:21:00 +0800

    187. 重复的DNA序列
    Signed-off-by: wuyuan.ok <wuyuan.ok@bytedance.com>

--- leetcode/03哈希表/187. 重复的DNA序列.go
+++ leetcode/03哈希表/187. 重复的DNA序列.go
@@ -7,10 +7,14 @@ package main
 // 例如，"ACGAATTCCG" 是一个 DNA序列 。
 // 在研究 DNA 时，识别 DNA 中的重复序列非常有用。
 //
-// 给定一个表示 DNA序列 的字符串 s ，返回所有在 DNA 分子中出现不止一次的 长度为 10 的序列(子字符串)。你可以按 任意顺序 返回答案。
+// 给定一个表示 DNA序列 的字符串 s ，返回所有在 DNA 分子中出现不止一次的 长度为 10 的序列(子字符串)。
+// 你可以按 任意顺序 返回答案。
 
 // findRepeatedDnaSequences .
-// 构造一个长度为10的窗口，不断向后滑动窗口，并将窗口所构成的字符串记录在哈希表中统计期出现的次数
+//
+// 滑动窗口
+// 构造一个长度为10的窗口，不断向后滑动窗口，并将窗口所构成的字符串记录在哈希表中统计其出现的次数，
+// 当发现一个窗口子串出现不止一次时将其计入结果集
 func findRepeatedDnaSequences(s string) []string {
 	result := make([]string, 0)
 	count := make(map[string]int)
