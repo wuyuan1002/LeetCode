09f48f81a81e29d095e0b40beaf307b0825672bb
wuyuan <2461190767@qq.com>
2024-10-15 16:27:24 +0800

    344. 反转字符串
    Signed-off-by: wuyuan.ok <wuyuan.ok@bytedance.com>

--- leetcode/04字符串/344. 反转字符串.go
+++ leetcode/04字符串/344. 反转字符串.go
@@ -6,16 +6,10 @@ package main
 // 不要给另外的数组分配额外的空间，你必须原地修改输入数组、使用 O(1) 的额外空间解决这一问题。
 
 // reverseString .
+//
 // 双指针
 func reverseString(s []byte) {
-	if s == nil || len(s) < 2 {
-		return
-	}
-
-	l, r := 0, len(s)-1
-	for l < r {
+	for l, r := 0, len(s)-1; l < r; l, r = l+1, r-1 {
 		s[l], s[r] = s[r], s[l]
-		l++
-		r--
 	}
 }
