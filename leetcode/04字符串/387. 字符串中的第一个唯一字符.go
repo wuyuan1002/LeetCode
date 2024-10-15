dc6f55f7effadf999f9f8ee1839dd32a244c902c
wuyuan <2461190767@qq.com>
2024-10-15 16:31:57 +0800

    387. 字符串中的第一个唯一字符
    Signed-off-by: wuyuan.ok <wuyuan.ok@bytedance.com>

--- leetcode/04字符串/387. 字符串中的第一个唯一字符.go
+++ leetcode/04字符串/387. 字符串中的第一个唯一字符.go
@@ -5,6 +5,8 @@ package main
 // 给定一个字符串 s ，找到 它的第一个不重复的字符，并返回它的索引 。如果不存在，则返回 -1 。
 
 // firstUniqChar .
+//
+// 两次遍历字符串，统计各个字符的出现次数，然后返回首个计数为1的字符下标
 func firstUniqChar(s string) int {
 	// 第一次遍历进行字符个数统计
 	count := [26]int{}
@@ -18,5 +20,6 @@ func firstUniqChar(s string) int {
 			return i
 		}
 	}
+
 	return -1
 }
