abac3fad39dd93ecbd08ee9d30c0986531ae20fd
wuyuan <2461190767@qq.com>
2024-10-15 16:38:22 +0800

    389. 找不同
    Signed-off-by: wuyuan.ok <wuyuan.ok@bytedance.com>

--- leetcode/04字符串/389. 找不同.go
+++ leetcode/04字符串/389. 找不同.go
@@ -8,8 +8,8 @@ package main
 // 请找出在 t 中被添加的字母。
 
 // findTheDifference .
-// 1. 分别将s和t的所有字符ascii码求和，两个和的差值就是添加的字符
-// 2. 分别对s和t中各个字符出现的次数进行计数，然后比较各个字符的计数
+//
+// 将s和t的所有字符ascii码求和，两个和的差值就是添加字符的ascii码
 func findTheDifference(s string, t string) byte {
 	sum := 0
 	for _, ch := range t {
