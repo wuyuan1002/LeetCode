448e63f301a6b16155e8dbd3d081d1bed3d7e319
wuyuan <2461190767@qq.com>
2024-10-16 17:18:57 +0800

    394. 字符串解码
    Signed-off-by: wuyuan.ok <wuyuan.ok@bytedance.com>

--- leetcode/04字符串/394. 字符串解码.go
+++ leetcode/04字符串/394. 字符串解码.go
@@ -7,13 +7,12 @@ import "strconv"
 // 给定一个经过编码的字符串，返回它解码后的字符串。
 //
 // 编码规则为: k[encoded_string]，表示其中方括号内部的 encoded_string 正好重复 k 次。注意 k 保证为正整数。
-//
 // 你可以认为输入字符串总是有效的；输入字符串中没有额外的空格，且输入的方括号总是符合格式要求的。
-//
 // 此外，你可以认为原始数据不包含数字，所有的数字只表示重复的次数 k ，例如不会出现像 3a 或 2[4] 的输入。
 
 // decodeString .
-// 使用栈，遇到左括号入栈，遇到右括号出栈
+//
+// 使用一个栈保存字符串及其出现次数，遇到左括号入栈，遇到右括号则出栈进行字符串展开
 func decodeString(s string) string {
 	// 栈内元素 -- 用于表示str重复num次
 	type entry struct {
@@ -23,13 +22,14 @@ func decodeString(s string) string {
 
 	// 定义栈
 	stack := make([]*entry, 0)
-	stack = append(stack, &entry{num: 1, str: ""}) // 先在栈内放一个元素，存最终的结果
+	// 先在栈内放一个元素，用来存整个字符串解码后的整体内容
+	stack = append(stack, &entry{num: 1, str: ""})
 
 	// 遍历字符串的每个字符
 	num := ""
 	for _, c := range s {
 		if c >= '0' && c <= '9' {
-			// 若是数字 -- 使用num记录下一个扩后内的数字出现几次
+			// 若是数字 -- 使用num记录下一个括号前的数字
 			num += string(c)
 		} else if c == '[' {
 			// 若是左括号 -- 入栈一个新元素，num表示该括号内的字符串循环几次
@@ -40,7 +40,7 @@ func decodeString(s string) string {
 			// 若是字母 -- 表示为当前栈顶元素的字符串内容，将字母加到栈顶元素上
 			stack[len(stack)-1].str += string(c)
 		} else if c == ']' {
-			// 若是右括号 -- 弹出一个栈顶元素，计算它的字符串，并把结果加到新的栈顶元素上
+			// 若是右括号 -- 弹出栈顶元素，计算它的字符串，并把结果加到新的栈顶元素上
 			top := stack[len(stack)-1]
 			stack = stack[:len(stack)-1]
 
@@ -50,11 +50,11 @@ func decodeString(s string) string {
 				top.str += str
 			}
 
-			// 将目标字符串加到新的栈顶元素的字符串上
+			// 将目标字符串加到新的栈顶元素的字符串上（因为当前元素字符串是新的栈顶元素的字符串的子集）
 			stack[len(stack)-1].str += top.str
 		}
 	}
 
-	// 遍历结束后，栈内只剩下刚开始存的一个元素，该元素的字符串内容就是结果
+	// 遍历结束后，栈内只剩下刚开始存的一个元素，该元素的字符串内容就是整个字符串解码后的整体内容
 	return stack[0].str
 }
