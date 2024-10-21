7b4e35b8f0ce889a25859d626377fb82913e2570
wuyuan <2461190767@qq.com>
2024-10-21 13:53:18 +0800

    94. 二叉树的中序遍历
    Signed-off-by: wuyuan.ok <wuyuan.ok@bytedance.com>

--- leetcode/06树/94. 二叉树的中序遍历.go
+++ leetcode/06树/94. 二叉树的中序遍历.go
@@ -4,7 +4,9 @@ package main
 
 // 给定一个二叉树的根节点 root ，返回 它的 中序 遍历 。
 
-// inorderTraversal 中序遍历二叉树
+// inorderTraversal .
+//
+// 左根右中序遍历二叉树
 func inorderTraversal(root *TreeNode) []int {
 	result := make([]int, 0)
 	dfsInOrder(root, &result)
