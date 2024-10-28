59a02e6eee75b18279a1a3389ae6123d49f187ee
wuyuan <2461190767@qq.com>
2024-10-28 19:34:33 +0800

    99. 恢复二叉搜索树
    Signed-off-by: wuyuan.ok <wuyuan.ok@bytedance.com>

--- leetcode/06树/99. 恢复二叉搜索树.go
+++ leetcode/06树/99. 恢复二叉搜索树.go
@@ -2,25 +2,27 @@ package main
 
 // 99. 恢复二叉搜索树
 
-// 给你二叉搜索树的根节点 root ，该树中的 恰好 两个节点的值被错误地交换。请在不改变其结构的情况下，恢复这棵树 。
+// 给你二叉搜索树的根节点 root ，该树中的 恰好 两个节点的值被错误地交换。
+// 请在不改变其结构的情况下，恢复这棵树 。
 
 // recoverTree .
-// 1. 中序遍历二叉树放入数组中，之后遍历数组，交换两个位置不正确的数字位置
-// 2. 二叉搜索树中序遍历是有序的，遍历过程中使用pre记录遍历到的前一个位置不正确的节点，再遍历到第二个不正确的节点时将其与第一个进行交换即可
+//
+// 二叉搜索树中序遍历是有序的，遍历过程中使用pre记录遍历到的前一个位置不正确的节点，
+// 再遍历到第二个不正确的节点时将其与第一个进行交换
 func recoverTree(root *TreeNode) {
 	// pre用来记录中序遍历过程中的前一个节点用来和下一个节点比较大小，errorNode用来记录位置不正确的两个节点
 	info := &Info{}
 
-	// 进行中序遍历查找两个位置错误的节点
+	// 中序遍历查找两个位置错误的节点
 	dfsRecoverTree(root, info)
 
-	// 若找到两个节点则进行值的交换
+	// 若找到两个位置错误的节点则进行值的交换
 	if info.errNode1 != nil && info.errNode2 != nil {
 		info.errNode1.Val, info.errNode2.Val = info.errNode2.Val, info.errNode1.Val
 	}
 }
 
-// dfsRecoverTree 中序遍历二叉树，将位置不正确的两个节点赋值
+// dfsRecoverTree 中序遍历二叉树，查找两个位置错误的节点并将其记录下来
 func dfsRecoverTree(node *TreeNode, info *Info) {
 	if node == nil {
 		return
@@ -46,7 +48,7 @@ func dfsRecoverTree(node *TreeNode, info *Info) {
 
 // Info 用于保存在递归过程中的节点信息
 type Info struct {
-	pre      *TreeNode
-	errNode1 *TreeNode
-	errNode2 *TreeNode
+	pre      *TreeNode // 遍历前一个节点
+	errNode1 *TreeNode // 第一个位置错误的节点
+	errNode2 *TreeNode // 最后一个位置错误的节点
 }
