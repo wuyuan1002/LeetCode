dff631377a018febaae099e3b942ccc8b624167c
wuyuan <2461190767@qq.com>
2024-11-04 20:23:17 +0800

    103. 二叉树的锯齿形层序遍历
    Signed-off-by: wuyuan.ok <wuyuan.ok@bytedance.com>

--- leetcode/06树/103. 二叉树的锯齿形层序遍历.go
+++ leetcode/06树/103. 二叉树的锯齿形层序遍历.go
@@ -8,8 +8,11 @@ import "container/list"
 // （即先从左往右，再从右往左进行下一层遍历，以此类推，层与层之间交替进行）。
 
 // zigzagLevelOrder .
-// 同 Offer 32、leetcode 102. 二叉树的层序遍历
-// 广度优先遍历二叉树 -- 使用一个双端队列存放遍历到的节点的左右节点，不断遍历该队列直至所有节点都被遍历完
+// leetcode 102. 二叉树的层序遍历
+// Offer 32 - 从上到下打印二叉树
+//
+// 广度优先遍历二叉树
+// 使用一个双端队列存放遍历到的节点的左右节点，不断遍历该队列直至所有节点都被遍历完
 // 遍历到偶数行时，从队头读，子节点按照左右顺序从队尾入队
 // 遍历到奇数行时，从队尾读，子节点按照右左顺序从队头入队
 func zigzagLevelOrder(root *TreeNode) [][]int {
@@ -21,32 +24,40 @@ func zigzagLevelOrder(root *TreeNode) [][]int {
 	queue := list.New()        // 使用一个双端队列存储遍历过程中的左右节点 -- 初始化先将root入队
 	queue.PushBack(root)
 
-	levelNum := 0         // 当前层数
-	for queue.Len() > 0 { // 若当前层有节点，则遍历该层
-		levelResult := make([]int, 0, queue.Len())     // 当前层的结果，因为在遍历的过程中遍历过的节点会从queue中删除，所以遍历完一层后queue内的节点就是下一层的所有节点
-		for count := queue.Len(); count > 0; count-- { // 开始遍历当前层，count：当前层节点个数
-			if levelNum%2 == 0 { // 偶数行 -- 从队头读，子节点按照左右顺序从队尾入队
-				// 获取当前层的下一个节点并出队列
+	// 队列不为空说明还有层没有遍历，不断遍历每一层，同时使用levelNum记录当前层的行号
+	for levelNum := 0; queue.Len() > 0; levelNum++ {
+
+		// 当前层的结果
+		levelResult := make([]int, 0, queue.Len())
+
+		// 开始遍历当前层，此时队列内的所有节点都是当前层的节点
+		// 因为在遍历的过程中遍历过的节点会从queue中删除，所以遍历完一层后队列内的节点就是下一层的所有节点
+		for count := queue.Len(); count > 0; count-- {
+
+			// 偶数行 -- 从队头读，子节点按照左右顺序从队尾入队
+			// 奇数行 -- 从队尾读，子节点按照右左顺序从队头入队
+			if levelNum%2 == 0 {
+				// 从队头获取当前层的下一个节点并出队列
 				node := queue.Remove(queue.Front()).(*TreeNode)
 
 				// 将当前节点入结果集
 				levelResult = append(levelResult, node.Val)
 
-				// 将当前节点的左右节点入队列 -- 为下一层的节点
+				// 将当前节点的子节点按照左右顺序从队尾入队 -- 为下一层的节点
 				if node.Left != nil {
 					queue.PushBack(node.Left)
 				}
 				if node.Right != nil {
 					queue.PushBack(node.Right)
 				}
-			} else { // 奇数行 -- 从队尾读，子节点按照右左顺序从队头入队
-				// 获取当前层的下一个节点并出队列
+			} else {
+				// 从队尾获取当前层的下一个节点并出队列
 				node := queue.Remove(queue.Back()).(*TreeNode)
 
 				// 将当前节点入结果集
 				levelResult = append(levelResult, node.Val)
 
-				// 将当前节点的右左节点入队列 -- 为下一层的节点
+				// 当前节点的子节点按照右左顺序从队头入队 -- 为下一层的节点
 				if node.Right != nil {
 					queue.PushFront(node.Right)
 				}
@@ -58,8 +69,6 @@ func zigzagLevelOrder(root *TreeNode) [][]int {
 
 		// 将当前行的结果入结果集
 		result = append(result, levelResult)
-		// 递增行号，准备遍历下一层
-		levelNum++
 	}
 
 	return result
