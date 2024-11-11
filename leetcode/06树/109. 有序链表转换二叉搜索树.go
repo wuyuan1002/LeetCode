0676158876138212360bd90aec0a8927eeff4821
wuyuan <2461190767@qq.com>
2024-11-11 10:42:23 +0800

    109. 有序链表转换二叉搜索树
    Signed-off-by: wuyuan.ok <wuyuan.ok@bytedance.com>

--- leetcode/06树/109. 有序链表转换二叉搜索树.go
+++ leetcode/06树/109. 有序链表转换二叉搜索树.go
@@ -6,18 +6,23 @@ package main
 //
 // 本题中，一个高度平衡二叉树是指一个二叉树每个节点 的左右两个子树的高度差不超过 1。
 
-// globalHead 用于指向当前正在构建的节点指针（当前的链表头节点），全局在整个链表上向后移动
+// globalHead 用于指向当前正在构建的节点指针，全局在整个链表上向后移动
 var globalHead *ListNode
 
 // sortedListToBST .
-// 1. 将链表中序遍历后得到有序数组，然后同 leetcode 108 进行解答
+// leetcode 108. 将有序数组转换为二叉搜索树
+//
+// *** 由于最终的树是平衡二叉树，所以根节点一定是链表的中间节点 ***
+//
+// 1. 将有序链表转换为有序数组，然后使用 leetcode 108 的方法进行解答
 // 2. 使用快慢指针法找到链表中间节点，此为当前树的根节点，然后递归构建其左子树和右子树
-// 3. 不先找到根节点再分别构建左右子树，而是使用中序遍历，先构建左子树再构建根节点再构建右子树，构建完左子树后自然会得到根节点是什么，不过需要提前确定树的大小（因为树是高度平衡的，根节点一定是中间节点）
+// 3. 不先找到根节点再分别构建左右子树，而是类似中序遍历，先构建左子树再构建根节点再构建右子树，
+//    构建完左子树后自然会得到根节点是什么，不过需要提前确定树的大小
 func sortedListToBST(head *ListNode) *TreeNode {
-	// 当前正在遍历的是第一个节点
+	// 从链表第一个节点开始向后挨个遍历进行构造
 	globalHead = head
 
-	// 求出链表长度 -- 整棵树的长度，也就是当前要求根节点的最右节点的下标
+	// 求出链表长度 -- 整棵树的长度，也就是当前根节点的最右子节点的下标
 	length := 0
 	for ; head != nil; head = head.Next {
 		length += 1
@@ -28,22 +33,24 @@ func sortedListToBST(head *ListNode) *TreeNode {
 }
 
 // buildTree 构建平衡二叉搜索树
-// left、right: 对于当前根节点的左右子节点在链表中的下标（左闭右闭）
+// left、right: 对于当前根节点的左右子节点在链表中的下标（左闭右闭 -- [left, right]）
 func buildTree(left, right int) *TreeNode {
 	if left > right {
 		return nil
 	}
 
-	// 计算当前根节点所处下标 -- 即根节点下标
+	// 当前根节点下标
 	mid := left + (right-left)/2
 
-	// 当前当前节点 -- 即根节点
+	// 先构建一个空的当前节点
 	root := &TreeNode{}
 
 	// 构建当前节点的左子树
 	root.Left = buildTree(left, mid-1)
 
-	// 构建当前节点，并将正在构造的节点指针向后移动一位 -- 即根节点
+	// 构建当前根节点，并将正在构造的节点指针globalHead向后移动一位
+	// globalHead表示当前正在构造的节点，在链表上从前向后移动，
+	// 在前面构造左子树时，它不断向后移动进行构造，到此处时mid前的节点都已构造完成
 	root.Val = globalHead.Val
 	globalHead = globalHead.Next
 
