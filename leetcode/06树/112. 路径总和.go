2d75b62155267458f183ce710afd98dcd9c5a56d
wuyuan <2461190767@qq.com>
2024-11-15 11:22:13 +0800

    112. 路径总和

--- leetcode/06树/112. 路径总和.go
+++ leetcode/06树/112. 路径总和.go
@@ -9,13 +9,16 @@ package main
 // 叶子节点 是指没有子节点的节点。
 
 // hasPathSum .
-// 同 leetcode 113. 路径总和 II、leetcode、437. 路径总和 III
-// 前序遍历二叉树，同时相加路径上的值判断是否满足路径总和
+// leetcode 113. 路径总和 II
+// leetcode、437. 路径总和 III
+//
+// 前序遍历二叉树，同时相加当前节点的值判断是否满足路径总和
+// 若某个节点恰好是叶子结点并且路径总和为目标值，则说明存在
 func hasPathSum(root *TreeNode, targetSum int) bool {
 	return dfsHasPathSum(root, 0, targetSum)
 }
 
-// dfsHasPathSum 前序遍历二叉树，判断给定节点是否为结果路径上的节点
+// dfsHasPathSum 前序遍历二叉树，判断当前节点是否满足路径总和规则
 func dfsHasPathSum(node *TreeNode, currentSum, targetSum int) bool {
 	if node == nil {
 		return false
@@ -24,7 +27,7 @@ func dfsHasPathSum(node *TreeNode, currentSum, targetSum int) bool {
 	// 将当前节点的值加到当前和上
 	currentSum += node.Val
 
-	// 若当前节点为叶子节点并且当前和为目标值，说明当前节点为结果路径上的节点
+	// 若当前节点为叶子节点并且当前和为目标值，说明当前找到了路径总和
 	if node.Left == nil && node.Right == nil && currentSum == targetSum {
 		return true
 	}
