4dbff03c627b61468adba3976304b3f02fca7d1d
wuyuan <2461190767@qq.com>
2024-11-18 16:32:43 +0800

    40. 组合总和 II

--- leetcode/07回溯/39. 组合总和.go
+++ leetcode/07回溯/39. 组合总和.go
@@ -11,15 +11,16 @@ package main
 //
 // 对于给定的输入，保证和为target 的不同组合数少于 150 个。
 
-// combinationSum .
+// combinationSum39 .
+// leetcode 40. 组合总和 II
 //
 // 递归回溯
-// 每次选定一个数字后进入下一层继续选择下一个数字到回溯路径，直到总和达到目标值说明找到一个解，
+// 每次选定一个数字后进入下一层继续选择下一个数字到回溯路径，直到总和达到目标值说明找到一个解
 // 记录结果后向前回溯，将回溯路径的数字不断重新选择，寻找其他的解并进行记录
 //
 // 此题也类似动态规划完全背包问题 -- leetcode 518. 零钱兑换
 // 不过动态规划只能求出解的个数，而无法求出每个解是什么
-func combinationSum(candidates []int, target int) [][]int {
+func combinationSum39(candidates []int, target int) [][]int {
 	if candidates == nil || len(candidates) == 0 {
 		return nil
 	}
@@ -27,18 +28,18 @@ func combinationSum(candidates []int, target int) [][]int {
 	res := make([]int, 0)      // 一次回溯过程中的结果 -- 回溯路径
 	result := make([][]int, 0) // 总结果集
 
-	dfsCombinationSum(candidates, 0, 0, target, &res, &result)
+	dfscombinationSum39(candidates, 0, 0, target, &res, &result)
 	return result
 }
 
-// dfsCombinationSum 回溯遍历选择列表，记录满足条件的结果
+// dfscombinationSum39 回溯遍历选择列表，记录满足条件的结果
 // candidates: 选择列表
 // start: 每次遍历的起始下标 -- 指定当前层的选择范围
 // currentSum: 当前和
 // target: 目标和 -- 终止条件
 // res: 一次回溯过程中的结果 -- 回溯路径
 // result: 总结果集
-func dfsCombinationSum(candidates []int, start int, currentSum, target int, res *[]int, result *[][]int) {
+func dfscombinationSum39(candidates []int, start int, currentSum, target int, res *[]int, result *[][]int) {
 	if currentSum == target {
 		// 回溯路径已满足条件 -- 将本次回溯的结果计入总结果集后返回
 		temp := make([]int, len(*res))
@@ -65,7 +66,7 @@ func dfsCombinationSum(candidates []int, start int, currentSum, target int, res 
 		// 递归进入下一层选择下一个数字
 		// 因为每个数字可以无限制重复被选取，所以进入下一层时当前数字仍然可以被使用，因此下一层的start为i
 		// 如果每个数字只能被使用一次，那么下一层的start应该传i+1
-		dfsCombinationSum(candidates, i, currentSum+candidates[i], target, res, result)
+		dfscombinationSum39(candidates, i, currentSum+candidates[i], target, res, result)
 		// 将当前值移出回溯路径
 		*res = (*res)[:len(*res)-1]
 	}
--- leetcode/07回溯/40. 组合总和 II.go
+++ leetcode/07回溯/40. 组合总和 II.go
@@ -1,5 +1,7 @@
 package main
 
+import "sort"
+
 // 40. 组合总和 II
 
 // 给定一个数组candidates和一个目标数target，找出candidates中所有可以使数字和为target的组合。
@@ -7,33 +9,40 @@ package main
 //
 // 注意：解集不能包含重复的组合。
 
-// combinationSum2
-// 1. 回溯法
-// 2. 动态规划 -- 01背包问题 -- 此题有重复元素，计算时无法去重
-func combinationSum2(candidates []int, target int) [][]int {
+// combinationSum40
+// leetcode 39. 组合总和
+//
+// 递归回溯
+// 由于选择列表中的数字存在重复，所以在数字选择过程中需要进行去重减枝
+// 每次选定一个数字后进入下一层继续选择下一个数字到回溯路径，直到总和达到目标值说明找到一个解
+// 记录结果后向前回溯，将回溯路径的数字不断重新选择，寻找其他的解并进行记录
+//
+// 此题也类似动态规划01背包问题 -- leetcode 416. 分割等和子集
+// 不过动态规划只能求出解的个数，而无法求出每个解是什么，且此题有重复元素，计算时需要去重
+func combinationSum40(candidates []int, target int) [][]int {
 	if candidates == nil || len(candidates) == 0 {
 		return nil
 	}
 
-	// 先排序方便剪枝
-	// 也可在dfs时使用visited记录已访问的数字来进行剪枝去重
-	// sort.Ints(candidates)
-	quickSort40(candidates, 0, len(candidates)-1)
+	// 先排序方便剪枝 -- 也可在dfs时使用visited记录已访问的数字来进行去重剪枝
+	sort.Ints(candidates)
 
 	res := make([]int, 0)      // 一次回溯过程中的结果 -- 回溯路径
 	result := make([][]int, 0) // 总结果集
-	dfsCombinationSum2(candidates, 0, 0, target, &res, &result)
+
+	dfscombinationSum40(candidates, 0, 0, target, &res, &result)
+
 	return result
 }
 
-// dfsCombinationSum2 回溯遍历选择列表，记录满足条件的结果
+// dfscombinationSum40 回溯遍历选择列表，记录满足条件的结果
 // candidates: 选择列表
 // start: 每次遍历的起始下标 -- 指定当前层的选择范围 -- candidates[start: ]
 // currentSum: 当前和
 // target: 目标和 -- 终止条件
 // res: 一次回溯过程中的结果 -- 回溯路径
 // result: 总结果集
-func dfsCombinationSum2(candidates []int, start int, currentSum, target int, res *[]int, result *[][]int) {
+func dfscombinationSum40(candidates []int, start int, currentSum, target int, res *[]int, result *[][]int) {
 	if currentSum == target {
 		// 回溯路径已满足条件 -- 将本次回溯的结果计入总结果集后返回
 		temp := make([]int, len(*res))
@@ -56,37 +65,11 @@ func dfsCombinationSum2(candidates []int, start int, currentSum, target int, res
 
 		// 固定当前层的一个元素到回溯路径
 		*res = append(*res, candidates[i])
-		// 递归进入下一层 -- 因为每个数字只能使用一次,
-		// 所以进入下一层时当前数字不可以继续使用, 因此下一层的start为 i+1
-		dfsCombinationSum2(candidates, i+1, currentSum+candidates[i], target, res, result)
+		// 递归进入下一层选择下一个数字
+		// 因为每个数字只能被使用一次，所以进入下一层时当前数字不可以继续使用，因此下一层的start为i+1
+		// 如果每个数字可以被使用多次，那么下一层的start应该传i
+		dfscombinationSum40(candidates, i+1, currentSum+candidates[i], target, res, result)
 		// 将当前元素移出回溯路径 -- 下一次循环选择下一个元素进行固定并继续进入下一层
 		*res = (*res)[:len(*res)-1]
 	}
-}
-
-// quickSort40 快排
-func quickSort40(nums []int, left, right int) {
-	if nums == nil || len(nums) == 0 || left >= right {
-		return
-	}
-
-	l, r := left, right
-	temp := nums[left]
-
-	for l < r {
-		for l < r && nums[r] >= temp {
-			r--
-		}
-		for l < r && nums[l] <= temp {
-			l++
-		}
-
-		if l < r {
-			nums[l], nums[r] = nums[r], nums[l]
-		}
-	}
-
-	nums[l], nums[left] = nums[left], nums[l]
-	quickSort40(nums, left, l-1)
-	quickSort40(nums, r+1, right)
 }
