81e68dcbd93b174ddefbda8b5a6dab1b7f164627
wuyuan <2461190767@qq.com>
2024-11-21 16:18:25 +0800

    226. 翻转二叉树

--- leetcode/06树/226. 翻转二叉树.go
+++ leetcode/06树/226. 翻转二叉树.go
@@ -5,7 +5,8 @@ package main
 // 给你一棵二叉树的根节点 root ，翻转这棵二叉树，并返回其根节点。
 
 // invertTree .
-// 同 Offer 27
+// Offer 27. 二叉树的镜像
+//
 // 前中后序遍历二叉树，把每个节点的左右子树交换
 func invertTree(root *TreeNode) *TreeNode {
 	if root == nil {
