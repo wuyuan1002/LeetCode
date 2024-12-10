e5acd375df3a61f08b4bdce6f0b868ee12d70f51
wuyuan <2461190767@qq.com>
2024-12-10 16:17:26 +0800

    230. 二叉搜索树中第K小的元素

--- leetcode/06树/230. 二叉搜索树中第K小的元素.go
+++ leetcode/06树/230. 二叉搜索树中第K小的元素.go
@@ -5,7 +5,8 @@ package main
 // 给定一个二叉搜索树的根节点 root ，和一个整数 k ，请你设计一个算法查找其中第 k 个最小元素（从 1 开始计数）。
 
 // kthSmallest .
-// 二叉搜索树的中序遍历是递增的，所以使用中序遍历二叉树，在遍历过程中查找第k小的数字，找到后终止遍历
+//
+// 二叉搜索树的中序遍历是递增的，所以中序遍历二叉树，在遍历过程中查找第k小的数字
 func kthSmallest(root *TreeNode, k int) int {
 	result := 0
 	dfsKthSmallest(root, &k, &result)
@@ -14,19 +15,16 @@ func kthSmallest(root *TreeNode, k int) int {
 
 // dfsKthSmallest 中序遍历二叉树寻找第k小的数字，找到后直接返回，不继续递归
 func dfsKthSmallest(node *TreeNode, k *int, result *int) {
-	if node == nil || *k == 0 {
+	// 若当前节点为nil或已经找到结果则直接返回
+	if node == nil || *k <= 0 {
 		return
 	}
 
 	dfsKthSmallest(node.Left, k, result)
 
-	// 将k减一，标记遍历的数字个数
-	*k--
-
-	// 若当前数字是第k小的数字，则将当前节点的值赋给结果，后直接返回，不继续递归
-	if *k == 0 {
+	// 递减计数，若当前数字正好是第k小则给结果赋值
+	if *k--; *k == 0 {
 		*result = node.Val
-		return
 	}
 
 	dfsKthSmallest(node.Right, k, result)
